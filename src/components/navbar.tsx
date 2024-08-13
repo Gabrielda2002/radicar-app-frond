8aafcfae0eacd9620ffd6e0f55b65aca949eb774
josting62 <jostingomez03@gmail.com>
2024-08-13 15:51:09 -0500

    se coloca funcionalidad de avatar con una barra de menu en el mismo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,22 @@
 import { NavLink } from "react-router-dom";
-
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 const Navbar = () => {
   const btnLink =
     "inline-block py-1 text-white hover:text-lime-400 cursor-pointer mr-4";
   const activeLink = "inline-block py-1 text-accent mr-4";
+
+  const user = {
+    name: "Tom Cook",
+    email: "tom@example.com",
+    imageUrl: "https://unavatar.io/josting62",
+  };
+
+  const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+  ];
+
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -33,24 +46,45 @@ const Navbar = () => {
             Registro
           </NavLink>
         </nav>
-        <button className="inline-flex items-center bg-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-100 rounded text-base text-white hover:text-black">
-          User
-          <svg
-            className="w-6 h-6 hover:text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-              clipRule="evenodd"
+        <Menu as="div" className="relative">
+          <MenuButton className="flex items-center bg-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base text-white hover:text-black">
+            <img
+              alt=""
+              src={user.imageUrl}
+              className="h-8 w-8 rounded-full mr-2"
             />
-          </svg>
-        </button>
+            <svg
+              className="w-6 h-6 hover:text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </MenuButton>
+          <MenuItems
+            transition
+            className="absolute right-0 mt-2 w-48 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          >
+            {userNavigation.map((item) => (
+              <MenuItem key={item.name}>
+                <a
+                  href={item.href}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-300 hover:rounded"
+                >
+                  {item.name}
+                </a>
+              </MenuItem>
+            ))}
+          </MenuItems>
+        </Menu>
       </div>
     </header>
   );
