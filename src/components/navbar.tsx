6d010bdd065438213654d12cb5c4220472faa703
josting62 <jostingomez03@gmail.com>
2024-08-15 16:35:02 -0500

    se reemplaza link de referencia de icono y se agrega funcion de ruta para el navlink en donde se reedirigue segun la opcion de menu hacia un modulo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { NavLink } from "react-router-dom";
+
+import userLogo from "/assets/user-logo.svg";
 
 const Navbar = () => {
   const user = {
@@ -8,38 +11,26 @@ const Navbar = () => {
   };
 
   const userNavigation = [
-    { name: "Perfil", href: "#" },
-    { name: "Cerrar Seccion", href: "#" },
+    { name: "Perfil", href: "/Perfil" },
+    { name: "Cerrar Seccion", href: "/Login" },
   ];
 
   return (
     <header className="text-gray-900 body-font">
       <div className="mx-auto flex flex-wrap p-5 bg-white border-b-2 border-black">
         <nav className="flex flex-wrap items-center text-base">
-          <img src="./src/imgs/logo-navbar.png" className="w-10 h-10" />
+          <NavLink to="/">
+            <img src="./src/imgs/logo-navbar.png" className="w-10 h-10" />
+          </NavLink>
         </nav>
         <Menu as="div" className="relative ml-auto">
-          <MenuButton className="flex items-center bg-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-color rounded text-base text-white hover:text-black">
+          <MenuButton className="flex items-center bg-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-color rounded text-base text-white hover:text-white group">
             <img
               alt=""
               src={user.imageUrl}
               className="h-8 w-8 rounded-full mr-2"
             />
-            <svg
-              className="w-6 h-6 hover:text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <img src={userLogo} alt="" className="w-8 h-8 group-hover:invert" />
           </MenuButton>
           <MenuItems
             transition
@@ -47,12 +38,11 @@ const Navbar = () => {
           >
             {userNavigation.map((item) => (
               <MenuItem key={item.name}>
-                <a
-                  href={item.href}
-                  className="block px-4 py-2 text-sm text-gray-800 hover:bg-color hover:text-white rounded-lg transition-colors duration-300"
-                >
-                  {item.name}
-                </a>
+                <NavLink to={item.href}>
+                  <a className="block px-4 py-2 text-sm text-gray-800 hover:bg-color hover:text-white rounded-lg transition-colors duration-300">
+                    {item.name}
+                  </a>
+                </NavLink>
               </MenuItem>
             ))}
           </MenuItems>
