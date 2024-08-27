1ddef35ea0ea0c07e1fcf7258680345b44a6f468
josting62 <jostingomez03@gmail.com>
2024-08-27 13:26:39 -0500

    (INCOMPLETO)se agrega variable dark para el efecto, tenga el texto en blanco

--- src/components/pages/inicio.tsx
+++ src/components/pages/inicio.tsx
@@ -5,7 +5,7 @@ const Inicio = () => {
     <section className="text-gray-600 body-font">
       <div className="container mx-auto">
         <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center border-2 rounded-full border-rose-500">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 ">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-gray-200">
             Bienvenidos a Nordvital
           </h1>
         </div>
