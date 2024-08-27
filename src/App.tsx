636c1a34c35944d61300de5b7190ce9d034afc3b
josting62 <jostingomez03@gmail.com>
2024-08-27 07:57:31 -0500

    se agrega propiedades para el efecto de darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useTheme } from "./context/blackWhiteContext"; // Importa el hook useTheme
 
 import SideBar from "./components/pages/sidebar";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Layout from "./components/layout";
 import Inicio from "./components/pages/inicio";
-import Modulos from "./components/pages/modulos";
 import Perfil from "./components/pages/perfil";
 
 // Importar los componentes de tablas
@@ -24,11 +24,13 @@ import TablaLugarRadicacion from "./components/pages/tablas-radicacion/tabla-lug
 import TablaIpsRemite from "./components/pages/tablas-radicacion/tabla-ips-remite";
 import TablaEspecialidad from "./components/pages/tablas-radicacion/tabla-especialidad";
 import TablaTipoServicio from "./components/pages/tablas-radicacion/tabla-tipo-servicio";
-
+import Usuarios from "./components/pages/usuarios";
 import Login from "./components/pages/login";
+import RegistrarUsuarios from "./components/pages/registrar-usuarios";
 
 export function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Inicializa en false para mostrar el login primero
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { theme, toggleTheme } = useTheme(); // Obtén el tema y la función de alternancia
 
   const handleLogout = () => {
     setIsAuthenticated(false);
@@ -39,7 +41,7 @@ export function App() {
   };
 
   return (
-    <div className="flex h-screen font-semibold">
+    <div className={`flex h-screen font-semibold ${theme === 'dark' ? 'dark' : ''}`}>
       <Routes>
         {isAuthenticated ? (
           <>
@@ -48,64 +50,29 @@ export function App() {
               element={
                 <div className="flex flex-row h-full w-full">
                   <SideBar />
-                  <div className="flex flex-col flex-grow bg-slate-200">
+                  <div className="flex flex-col flex-grow bg-slate-200 dark:bg-gray-900">
                     <Navbar onLogout={handleLogout} />
                     <div className="flex-grow overflow-auto">
                       <Layout>
                         <Routes>
                           <Route path="/" element={<Inicio />} />
-                          <Route path="/modulos" element={<Modulos />} />
                           <Route path="/tabla" element={<Tabla />} />
-                          <Route
-                            path="/tabla-auditoria"
-                            element={<TablaAuditoria />}
-                          />
-                          <Route
-                            path="/tabla-cirugias"
-                            element={<TablaCirugias />}
-                          />
+                          <Route path="/tabla-auditoria" element={<TablaAuditoria />} />
+                          <Route path="/tabla-cirugias" element={<TablaCirugias />} />
                           <Route path="/tabla-cups" element={<TablaCups />} />
-                          <Route
-                            path="/tabla-pacientes"
-                            element={<TablaPacientes />}
-                          />
-                          <Route
-                            path="/tabla-radicadores"
-                            element={<TablaRadicadores />}
-                          />
-                          <Route
-                            path="/tabla-municipios"
-                            element={<TablaMunicipios />}
-                          />
-                          <Route
-                            path="/tabla-convenios"
-                            element={<TablaConvenios />}
-                          />
-                          <Route
-                            path="/tabla-tipo-documento"
-                            element={<TablaTipoDocumento />}
-                          />
-                          <Route
-                            path="/tabla-ips-primaria"
-                            element={<TablaIpsPrimaria />}
-                          />
-                          <Route
-                            path="/tabla-lugar-radicacion"
-                            element={<TablaLugarRadicacion />}
-                          />
-                          <Route
-                            path="/tabla-ips-remite"
-                            element={<TablaIpsRemite />}
-                          />
-                          <Route
-                            path="/tabla-especialidad"
-                            element={<TablaEspecialidad />}
-                          />
-                          <Route
-                            path="/tabla-tipo-servicio"
-                            element={<TablaTipoServicio />}
-                          />
+                          <Route path="/tabla-pacientes" element={<TablaPacientes />} />
+                          <Route path="/tabla-radicadores" element={<TablaRadicadores />} />
+                          <Route path="/tabla-municipios" element={<TablaMunicipios />} />
+                          <Route path="/tabla-convenios" element={<TablaConvenios />} />
+                          <Route path="/tabla-tipo-documento" element={<TablaTipoDocumento />} />
+                          <Route path="/tabla-ips-primaria" element={<TablaIpsPrimaria />} />
+                          <Route path="/tabla-lugar-radicacion" element={<TablaLugarRadicacion />} />
+                          <Route path="/tabla-ips-remite" element={<TablaIpsRemite />} />
+                          <Route path="/tabla-especialidad" element={<TablaEspecialidad />} />
+                          <Route path="/tabla-tipo-servicio" element={<TablaTipoServicio />} />
                           <Route path="/perfil" element={<Perfil />} />
+                          <Route path="/Usuarios" element={<Usuarios />} />
+                          <Route path="/RegistrarUsuarios" element={<RegistrarUsuarios />} />
                           <Route path="*" element={<Navigate to="/" />} />
                         </Routes>
                       </Layout>
@@ -118,10 +85,7 @@ export function App() {
             <Route path="*" element={<Navigate to="/" />} />
           </>
         ) : (
-          <Route
-            path="*"
-            element={<Login onLoginSuccess={handleLoginSuccess} />}
-          />
+          <Route path="*" element={<Login onLoginSuccess={handleLoginSuccess} />} />
         )}
       </Routes>
     </div>
