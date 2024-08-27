fe79d8cdb40899787d14a719731d0db457612529
josting62 <jostingomez03@gmail.com>
2024-08-27 13:18:07 -0500

    se elimina el contenido sin usar para reemplazarlo por el scripr del instagram

--- src/components/pages/inicio.tsx
+++ src/components/pages/inicio.tsx
@@ -1,3 +1,5 @@
+import ExternalScriptComponent from "../ExternalScriptComponent"; // Asegúrate de que la ruta sea correcta
+
 const Inicio = () => {
   return (
     <section className="text-gray-600 body-font">
@@ -7,114 +9,10 @@ const Inicio = () => {
             Bienvenidos a Nordvital
           </h1>
         </div>
-        <div className="flex flex-wrap -m-4">
-          <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-900 p-6 rounded-lg bg-white">
-              <div className="flex flex-col sm:flex-row sm:justify-between items-center mb-4">
-                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-2 sm:mb-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div className="sm:ml-auto">
-                  <svg
-                    fill="currentColor"
-                    className="w-6 h-6 text-gray-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-                  </svg>
-                </div>
-              </div>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                Shooting Stars
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-          <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-900 p-6 rounded-lg bg-white">
-              <div className="flex flex-col sm:flex-row sm:justify-between items-center mb-4">
-                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-2 sm:mb-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div className="sm:ml-auto">
-                  <svg
-                    fill="currentColor"
-                    className="w-6 h-6 text-gray-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-                  </svg>
-                </div>
-              </div>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                Shooting Stars
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
 
-          <div className="xl:w-1/3 md:w-1/2 p-4">
-            <div className="border border-gray-900 p-6 rounded-lg bg-white">
-              <div className="flex flex-col sm:flex-row sm:justify-between items-center mb-4">
-                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 mb-2 sm:mb-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div className="sm:ml-auto">
-                  <svg
-                    fill="currentColor"
-                    className="w-6 h-6 text-gray-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-                  </svg>
-                </div>
-              </div>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                Shooting Stars
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waist co, subway tile
-                poke farm.
-              </p>
-            </div>
-          </div>
-        </div>
+        {/* Aquí es donde insertas el componente que carga el script externo */}
+        <ExternalScriptComponent />
+
       </div>
     </section>
   );
