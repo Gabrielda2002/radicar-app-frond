df483c5420574465b9af899a797200e5e9a2ae8c
josting62 <jostingomez03@gmail.com>
2024-08-27 15:39:12 -0500

    espacio

--- src/context/blackWhiteContext.tsx
+++ src/context/blackWhiteContext.tsx
@@ -1,17 +1,20 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+// context/blackWhiteContext.tsx
+import React, { createContext, useContext, useState, ReactNode } from "react";
 
 interface ThemeContextType {
-  theme: 'light' | 'dark';
+  theme: string;
   toggleTheme: () => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<string>("light");
 
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
@@ -21,10 +24,10 @@ export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
   );
 };
 
-export const useTheme = () => {
+export const useTheme = (): ThemeContextType => {
   const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider');
+    throw new Error("useTheme must be used within a ThemeProvider");
   }
   return context;
 };
