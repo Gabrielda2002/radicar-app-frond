4d2d72f8f8c282f3c2c946a38a3e3c337447d55a
josting62 <jostingomez03@gmail.com>
2024-08-29 17:16:57 -0500

    se agregan modales para las tablas con sus respectivas propiedades

--- src/components/pages/modals/modal-convenio.tsx
+++ src/components/pages/modals/modal-convenio.tsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 
-const ModalMunicipios = () => {
+const ModalConvenio = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [showAnimation, setShowAnimation] = useState(false);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
+
   useEffect(() => {
     if (isOpen) {
       setShowAnimation(true);
+    } else {
+      setShowAnimation(false);
       setTimeout(() => {
         setShowAnimation(false);
-      }, 1000);
+      }, 300);
     }
-    [isOpen];
-  });
+  }, [isOpen]);
 
   return (
     <>
@@ -23,7 +25,7 @@ const ModalMunicipios = () => {
         onClick={toggleModal}
         className="border-2 w-[150px] h-10 rounded-md focus:outline-none bg-color text-white hover:bg-emerald-900 active:bg-emerald-800"
       >
-        Agregar Municipios
+        Agregar Convenio
       </button>
       {isOpen && (
         <div className="fixed z-50 flex items-center justify-center transition-opacity duration-300 bg-black bg-opacity-40 -inset-5 backdrop-blur-sm">
@@ -32,55 +34,46 @@ const ModalMunicipios = () => {
             className="fixed inset-0 transition-opacity duration-300 bg-black opacity-40 backdrop-blur-sm"
           ></div>
 
-          {/* Contenido del formulario */}
+          {/* Contenido del Formulario */}
 
           <div className="z-10 w-[900px] p-10 bg-white rounded shadow-lg transform transition-transform duration-300 dark:bg-gray-800">
-            <div className="flex justify-between mb-10 items center">
+            <div className="flex items-center justify-between mb-10">
               <h2 className="text-lg font-semibold text-color">
-                Agregar Municipios
+                Agregar Convenio
               </h2>
               <button
                 onClick={toggleModal}
-                className="text-gray-500 hover:text-gray-700"
+                className="text-gray-500 hover-gray-700"
               >
                 &times;
               </button>
             </div>
 
-            {/* Contenido del Fomrulario */}
+            {/* Contenido del Formulario */}
 
-            <div className="grid grid-cols-2 gap-10 mb-10">
+            <div className="grid grid-cols-1 gap-10 mb-10">
               <div>
-                <label className="block mb-2 font-bold text-gray-700 dark:text-gray-200">
-                  Nombre Municipio
+                <label className="block mb-2 font-bold text-gray-7000 dark:text-gray-200">
+                  Nombre:
                 </label>
                 <input
                   type="text"
-                  placeholder="Ingrese Municipio"
-                  className="w-full px-3 py-2 border border-gray-300 rounded dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                />
-              </div>
-
-              <div className="block mb-2 font-bold text-gray-700 dark:text-gray-200">
-                <label className="block mb-2 font-bold text-gray700 dark:text-gray-200">
-                  NIT Municipios
-                </label>
-                <input
-                  type="number"
-                  placeholder="Ingrese NIT..."
-                  className="w-full px-3 py-2 border border-gray-300 rounded dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+                  placeholder="Ingrese Nombre Convenio..."
+                  className="w-full px-3 py-2 border border-gray-200 rounded dark:border-gray-600 dark:bg-gray-700 dark:text-white"
                 />
               </div>
             </div>
+
             {/* Botones */}
+
             <div className="flex justify-end space-x-3">
               <button
                 onClick={toggleModal}
-                className="px-4 py-2 text-white bg-gray-600 rounded-lg hover:bg-slate-600"
+                className="px-4 py-2 text-white bg-gray-500 rounded-lg round hover:bg-gray-600"
               >
                 Cerrar
               </button>
-              <button className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 ">
+              <button className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
                 Agregar
               </button>
             </div>
@@ -91,4 +84,4 @@ const ModalMunicipios = () => {
   );
 };
 
-export default ModalMunicipios;
+export default ModalConvenio;
