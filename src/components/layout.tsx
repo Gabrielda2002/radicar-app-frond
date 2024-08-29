32147fc8b7c5271e0666a6da8e3d40e833d49a6e
josting62 <jostingomez03@gmail.com>
2024-08-29 09:46:29 -0500

    se ajusta espacio

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 const Layout = (props: any) => {
   return (
     <>
-      <section className="px-12 pt-10 mx-auto max-w-screen">
+      <section className="w-full px-12 pt-10 mx-auto">
         {props.children}
       </section>
     </>
