e57c5792c61c6c826524d8e7065369f7e6323abe
josting62 <jostingomez03@gmail.com>
2024-09-10 14:52:54 -0500

    ordenamiento de componentes y de validacion de contenedores para mayor fluides y atacamiento de jerarquía

--- src/components/pages/SSGC/FolderList.tsx
+++ src/components/pages/SSGC/FolderList.tsx
@@ -11,30 +11,45 @@ interface FolderListProps {
   folders: Folder[];
   onFolderClick: (folderId: string, folderName: string) => void;
   onDelete: (id: string, type: "carpetas" | "archivo") => void;
-  renameItem: (id: string, newName: string, type: "carpetas" | "archivo") => void;
+  renameItem: (
+    id: string,
+    newName: string,
+    type: "carpetas" | "archivo"
+  ) => void;
 }
 
 const FolderList: React.FC<FolderListProps> = ({
   folders,
   onFolderClick,
   onDelete,
-  renameItem
+  renameItem,
 }) => {
   return (
     <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 md:grid-cols-4">
       {folders.map((folder) => (
         <div
           key={folder.id}
-          // onClick={() => onFolderClick(folder.id, folder.name)}
-          className="flex flex-col items-center p-4 bg-gray-100 rounded-md cursor-pointer dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600"
+          onClick={() => onFolderClick(folder.id, folder.name)} // Se abre la carpeta
+          className="relative flex flex-col items-center p-4 text-gray-700 bg-gray-100 rounded-md cursor-pointer dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-300 dark:hover:text-indigo-500 dark:hover:underline"
         >
-          {/* <h1>Carpetas</h1> */}
+          {/* Icono de la carpeta */}
           <img src={folderIcon} alt="folder-icon" className="w-16 h-16 mb-2" />
-          <span onClick={() => onFolderClick(folder.id, folder.name)}>
-            <p className="text-sm font-medium text-center text-gray-700 dark:text-gray-300">{folder.name}</p>
-          </span>
-          <ItemManu onDelete={() => onDelete(folder.id, "carpetas")} renameItem={(newName: string) => renameItem(folder.id, newName, "carpetas")}/>
-          {/* <button onClick={() => onDelete(folder.id, "carpetas")}>Eliminar</button> */}
+
+          {/* Menú en la esquina superior derecha */}
+          <div
+            className="absolute top-0 right-0 mt-2 mr-2"
+            onClick={(e) => e.stopPropagation()} // Evitar que el clic aquí abra la carpeta
+          >
+            <ItemManu
+              onDelete={() => onDelete(folder.id, "carpetas")}
+              renameItem={(newName: string) =>
+                renameItem(folder.id, newName, "carpetas")
+              }
+            />
+          </div>
+
+          {/* Nombre de la carpeta */}
+          <p className="text-sm font-medium text-center ">{folder.name}</p>
         </div>
       ))}
     </div>
