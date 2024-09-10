15b5d8a3579e722e6e9aaf7594d810df9553fe7e
josting62 <jostingomez03@gmail.com>
2024-09-10 14:51:00 -0500

    se agregan funciones y constantes para la funcion de validacion de la carpeta para la funcion del menu

--- src/components/pages/SSGC/DropDownManu.tsx
+++ src/components/pages/SSGC/DropDownManu.tsx
@@ -1,24 +1,30 @@
-import  React, { useState } from "react";
+import React, { useState } from "react";
 import ModalCrearCarpeta from "../modals/ModalCrearCarpeta";
-
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
 import ModalSubirArchivo from "../modals/ModalSubirArchivo";
 import { useUploadFile } from "../../../hooks/useUploadFile";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 
 interface DropDownManuProps {
   currentFolderId: string;
   uploadNewFile: (formData: FormData, id: string | number) => Promise<void>;
   createNewFolder: (name: string) => void;
+  isInFolder: boolean;
 }
 
-const DropDownManu: React.FC<DropDownManuProps> = ({uploadNewFile, currentFolderId, createNewFolder}) => {
-  //   const [isOpen, setIsOpen] = useState(false);
+const DropDownManu: React.FC<DropDownManuProps> = ({
+  uploadNewFile,
+  currentFolderId,
+  createNewFolder,
+  isInFolder,
+}) => {
   const [stadOpenFolder, setStadOpenFolder] = useState(false);
   const [stadOpenFile, setStadOpenFile] = useState(false);
-  
-  const {  uploading, handleFileChange, handleUpload } =
-  useUploadFile(uploadNewFile, currentFolderId)
+
+  const { uploading, handleFileChange, handleUpload } = useUploadFile(
+    uploadNewFile,
+    currentFolderId
+  );
 
   const toggleModalFolder = () => {
     setStadOpenFolder(!stadOpenFolder);
@@ -26,64 +32,72 @@ const DropDownManu: React.FC<DropDownManuProps> = ({uploadNewFile, currentFolder
 
   const toggleModalFile = () => {
     setStadOpenFile(!stadOpenFile);
-    
   };
 
   const handleModalOpenFolder = () => {
     setStadOpenFolder(true);
-    // setStadOpenFile(true);
-    // setIsOpen(false);
   };
+
   const handleModalOpenFile = () => {
     setStadOpenFile(true);
-    // setStadOpenFile(true);
-    // setIsOpen(false);
   };
 
   return (
     <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Nuevo
-          <ChevronDownIcon
-            arial-hidden="true"
-            className="-mr-1 h-5 w-5 text-gray-400"
+      {({ open }) => (
+        <>
+          <div>
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white dark:bg-slate-700 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 group">
+              Nuevo
+              <ChevronDownIcon
+                aria-hidden="true"
+                className={`w-5 h-5 -mr-1 text-gray-400 duration-300 transform ${
+                  open ? "rotate-180" : ""
+                }`}
+              />
+            </MenuButton>
+          </div>
+
+          <MenuItems
+            transition
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              <MenuItem>
+                <button
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 w-full text-left dark:text-gray-200 dark:data-[focus]:bg-gray-500"
+                  onClick={handleModalOpenFolder}
+                >
+                  Crear Carpeta.
+                </button>
+              </MenuItem>
+              {isInFolder && (
+                <MenuItem>
+                  <button
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 w-full text-left dark:text-gray-200 dark:data-[focus]:bg-gray-500"
+                    onClick={handleModalOpenFile}
+                  >
+                    Subir Archivo.
+                  </button>
+                </MenuItem>
+              )}
+            </div>
+          </MenuItems>
+
+          <ModalCrearCarpeta
+            standOpen={stadOpenFolder}
+            toggleModal={toggleModalFolder}
+            createNewFolder={createNewFolder}
           />
-        </MenuButton>
-      </div>
-
-      <MenuItems
-        transition
-        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <div className="py-1">
-          <MenuItem>
-            <button
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-              onClick={handleModalOpenFolder}
-            >
-              Crear Carpeta.
-            </button>
-          </MenuItem>
-          <MenuItem>
-            <button
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-              onClick={handleModalOpenFile}
-            >
-              Subir Archivo
-            </button>
-          </MenuItem>
-        </div>
-      </MenuItems>
-
-      {<ModalCrearCarpeta standOpen={stadOpenFolder} toggleModal={toggleModalFolder} createNewFolder={createNewFolder} />}
-      {<ModalSubirArchivo
+          <ModalSubirArchivo
             onFileChange={handleFileChange}
             onUpload={handleUpload}
             uploading={uploading}
             stadopen={stadOpenFile}
             toggleModal={toggleModalFile}
-          />}
+          />
+        </>
+      )}
     </Menu>
   );
 };
