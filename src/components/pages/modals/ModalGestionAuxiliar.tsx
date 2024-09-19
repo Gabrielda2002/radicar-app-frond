1abb3db67c246b8b974c6fbf71f69750c51663e1
josting62 <jostingomez03@gmail.com>
2024-09-19 15:10:32 -0500

    se elimina propiedades sin usar

--- src/components/pages/modals/ModalGestionAuxiliar.tsx
+++ src/components/pages/modals/ModalGestionAuxiliar.tsx
@@ -17,11 +17,6 @@ const ModalGestionAuxiliar: React.FC<ModalGestionAuxiliarProps> = ({
 }) => {
   const [stadOpen, setStadOpen] = useState(false); // Estados Auxiliar
   const [openServicio, setOpenServicio] = useState(false); // Estados Servicios
-  const { showAnimation, closing } = useAnimation(
-    stadOpen,
-    () => setStadOpen(false),
-    300
-  );
 
   if (!isOpen || !radicacion) return null;
 
