b33456110fb6b9aabbbdd20855ec056c8e9eaee9
wilmerjb <166762758+wilmerjb@users.noreply.github>
2024-09-19 10:49:58 -0500

    mejorando style ModalReporte

--- src/components/pages/modals/ModalReporte.tsx
+++ src/components/pages/modals/ModalReporte.tsx
@@ -50,145 +50,160 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, formType }) => {
 
   return (
     <div
-      className={`fixed z-50 flex items-center justify-center bg-black -inset-5 bg-opacity-40 transition-opacity duration-300 backdrop-blur-sm ${
+      className={`fixed z-50 flex pt-20 justify-center bg-black -inset-5 bg-opacity-40 transition-opacity duration-300 backdrop-blur-sm ${
         showAnimation && !closing ? "opacity-100" : "opacity-0"
       }`}
       onClick={handleOutsideClick}
     >
-      <div
-        className={`w-auto p-10 bg-white rounded-lg shadow-lg dark:bg-gray-900 transform transition-transform duration-300 ${
-          showAnimation && !closing
-            ? "translate-y-0 opacity-100"
-            : "translate-y-10 opacity-0"
-        }`}
-      >
-        <div className="flex justify-end">
-          <button
-            onClick={() => {
-              setShowSecondModal(false);
-              setTimeout(onClose, 300);
-            }}
-          >
-            <img className="w-5 h-5 dark:invert" src={close} alt="close" />
-          </button>
-        </div>
-        <div className="flex flex-row">
-          <h2 className="mb-6 text-xl font-semibold text-color dark:text-white">
-            {modalTitle}
-          </h2>
-        </div>
-        <form onSubmit={handleSubmit}>
-          {!showSecondModal ? (
-            <>
-              <div className="mb-10">
-                <label className="block mb-5 text-lg font-medium text-blue-500 dark:text-white">
-                  Seleccione la fecha con la que desea hacer el filtrado
-                </label>
-                <select
-                  name="reportOptions"
-                  value={formValues.reportOptions}
-                  onChange={handleChange}
-                  className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                >
-                  <option value="">SELECCIONE</option>
-                  <option value="Autorizacion">Autorizacion</option>
-                  <option value="Radicacion">Radicacion</option>
-                </select>
-              </div>
-            </>
-          ) : (
-            <div className="px-4">
-              <button
-                type="button"
-                onClick={() => setShowSecondModal(false)}
-                className="flex items-center mb-6"
-              >
-                <img className="w-8 h-8 dark:invert" src={back} alt="back" />
-              </button>
-              <div className="mb-10">
-                <label className="block mb-5 text-lg font-medium text-blue-500 dark:text-white">
-                  {formValues.reportOptions === "Autorizacion"
-                    ? "Código de Autorizacion"
-                    : "Código de Radicacion"}
-                </label>
-                <div className="grid grid-cols-2 gap-4">
-                  <div>
-                    <label className="block mb-2 text-gray-700 dark:text-gray-300">
-                      Inicio
-                    </label>
-                    <input
-                      type="date"
-                      name="dateStartRadicado"
-                      onChange={(e) => setDateStartRadicado(e.target.value)}
-                      className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                    />
-                  </div>
-                  <div>
-                    <label className="block mb-2 text-gray-700 dark:text-gray-300">
-                      Fin
-                    </label>
-                    <input
-                      type="date"
-                      name="dateEndRadicado"
-                      onChange={(e) => setDateEndRadicado(e.target.value)}
-                      className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-          <div className="mb-4">
-            <label className="block mb-4 text-xl font-semibold text-color dark:text-white">
-              Código CUPS:
-            </label>
-            <input
-              type="text"
-              name="cupsCode"
-              onChange={(e) => {
-                handleChange(e);
-                setCupsCode(e.target.value);
-              }}
-              placeholder="Ingrese código..."
-              className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-            />
-          </div>
-          <div className="flex justify-end space-x-2">
+      <section>
+        <div
+          className={`w-[600px]  bg-white rounded-md  overflow-hidden shadow-lg dark:bg-gray-900 transform transition-transform duration-300 ${
+            showAnimation && !closing
+              ? "translate-y-0 opacity-100"
+              : "translate-y-10 opacity-0"
+          }`}
+        >
+          {/* container-header */}
+          <div className="flex items-center justify-between  px-2 py-2 dark:bg-gray-900">
+            <h2 className="text-xl font-semibold text-color dark:text-gray-200  ">
+              {modalTitle}
+            </h2>
             <button
-              type="button"
+              className="text-xl text-gray-500 hover-gray-700 pr-2"
               onClick={() => {
                 setShowSecondModal(false);
                 setTimeout(onClose, 300);
               }}
-              className="px-4 py-2 text-white bg-gray-500 rounded-lg hover:bg-gray-600"
             >
-              Cerrar
+              &times;
             </button>
-            {!showSecondModal ? (
-              <button
-                onClick={() => setShowSecondModal(true)}
-                className={`px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700 ${
-                  !formValues.reportOptions
-                    ? "opacity-50 cursor-not-allowed"
-                    : ""
-                }`}
-                disabled={!formValues.reportOptions} // Deshabilita si no hay opción seleccionada
-              >
-                Generar
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
-                onClick={handleDownloadReport}
-              >
-                Descargar
-              </button>
-            )}
           </div>
-          {error && <p className="mt-4 text-red-500">{error}</p>}
-        </form>
-      </div>
+
+          {/* init-form */}
+          <form onSubmit={handleSubmit} className="">
+            {!showSecondModal ? (
+              <>
+                <div className="mb-8 px-6 mt-2">
+                  <label className="flex mb-2  text-xl font-medium text-blue-500 dark:text-white">
+                    Seleccione la fecha con la que desea hacer el filtrado
+                  </label>
+                  <select
+                    name="reportOptions"
+                    value={formValues.reportOptions}
+                    onChange={handleChange}
+                    className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+                  >
+                    <option value="">SELECCIONE</option>
+                    <option value="Autorizacion">Autorización</option>
+                    <option value="Radicacion">Radicación</option>
+                  </select>
+                </div>
+              </>
+            ) : (
+              // secction-option
+              <div className="px-4">
+                <div className="flex ">
+                  <div className=" w-10 pb-1 -ms-4">
+                    <img
+                      src={back}
+                      alt="back"
+                      onClick={() => setShowSecondModal(false)}
+                      className="cursor-pointer w-8 "
+                    />
+                  </div>
+
+                  <label className="block mb-5 text-xl font-medium text-blue-500 dark:text-white">
+                    {formValues.reportOptions === "Autorizacion"
+                      ? "Código de Autorización"
+                      : "Código de Radicación"}
+                  </label>
+                </div>
+
+                <div className="mb-8">
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <label className="block mb-2 text-gray-700 dark:text-gray-300">
+                        Inicio
+                      </label>
+                      <input
+                        type="date"
+                        name="dateStartRadicado"
+                        onChange={(e) => setDateStartRadicado(e.target.value)}
+                        className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+                      />
+                    </div>
+                    <div>
+                      <label className="block mb-2 text-gray-700 dark:text-gray-300">
+                        Fin
+                      </label>
+                      <input
+                        type="date"
+                        name="dateEndRadicado"
+                        onChange={(e) => setDateEndRadicado(e.target.value)}
+                        className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {/* section-two */}
+            <div className="mb-6 px-2">
+              <label className="flex mb-2 text-xl font-semibold text-color dark:text-white">
+                Código CUPS :
+              </label>
+              <input
+                type="text"
+                name="cupsCode"
+                onChange={(e) => {
+                  handleChange(e);
+                  setCupsCode(e.target.value);
+                }}
+                placeholder="Ingrese código..."
+                className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring dark:ring-gray-500 dark:focus:bg-slate-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+              />
+            </div>
+
+            {/* container-footer */}
+            <div className="flex justify-end space-x-2 py-4 px-2">
+              <button
+                type="button"
+                onClick={() => {
+                  setShowSecondModal(false);
+                  setTimeout(onClose, 300);
+                }}
+                className="px-4 py-2 text-white bg-gray-500 rounded-lg hover:bg-gray-600"
+              >
+                Cerrar
+              </button>
+              {!showSecondModal ? (
+                <button
+                  onClick={() => setShowSecondModal(true)}
+                  className={`px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700 ${
+                    !formValues.reportOptions
+                      ? "opacity-50 cursor-not-allowed"
+                      : ""
+                  }`}
+                  disabled={!formValues.reportOptions} // Deshabilita si no hay opción seleccionada
+                >
+                  Generar
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+                  onClick={handleDownloadReport}
+                >
+                  Descargar
+                </button>
+              )}
+            </div>
+
+            {error && <p className="mt-4 text-red-500">{error}</p>}
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
