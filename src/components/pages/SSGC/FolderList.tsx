ef9c733b66d7edde8d3303d64dfa4f9bfb03a598
wilmerjb <166762758+wilmerjb@users.noreply.github>
2024-09-23 14:28:35 -0500

    solucionando conflicto "estructura" (falta icon )

--- src/components/pages/SSGC/FolderList.tsx
+++ src/components/pages/SSGC/FolderList.tsx
@@ -66,13 +66,13 @@ const FolderList: React.FC<FolderListProps> = ({
         <div
           key={folder.id}
           onClick={() => onFolderClick(folder.id, folder.name)} // Se abre la carpeta
-          className="relative flex flex-col items-center p-4 text-gray-700 bg-gray-200 rounded-md cursor-pointer dark:bg-gray-700 hover:bg-blue-200 dark:hover:bg-gray-600 dark:text-gray-300 dark:hover:text-indigo-500 dark:hover:underline"
+          className=" flex flex-col items-center p-4 text-gray-700 bg-gray-200 rounded-md cursor-pointer dark:bg-gray-700 hover:bg-blue-200 dark:hover:bg-gray-600 dark:text-gray-300 dark:hover:text-indigo-500 dark:hover:underline"
         >
           {/* Icono de la carpeta */}
           <img src={folderIcon} alt="folder-icon" className="w-16 h-16 mb-2" />
           {/* Menú en la esquina superior derecha */}
           <div
-            className="absolute top-0 right-0 mt-2 mr-2"
+            className=""
             onClick={(e) => e.stopPropagation()} // Evitar que el clic aquí abra la carpeta
           >
             <ItemManu
