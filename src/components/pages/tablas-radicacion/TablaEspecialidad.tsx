fa2d02bc3f573b3d1aec3e84f8c8278d7cb975bc
Gabriel Duarte <duartearias2002@gmail.com>
2024-09-27 16:23:58 -0500

    agregar tipo servicios
    Se hace pequeno ajuste en tabla especialidad

--- src/components/pages/tablas-radicacion/TablaEspecialidad.tsx
+++ src/components/pages/tablas-radicacion/TablaEspecialidad.tsx
@@ -6,7 +6,6 @@ import ModalAction from "../modals/ModalAction";
 import useSearch from "../../../hooks/useSearch";
 import LoadingSpinner from "../../LoadingSpinner";
 import usePagination from "../../../hooks/usePagination";
-import ModalEspecialidad from "../modals/ModalEspecialidad";
 import { useFetchEspecialidad } from "../../../hooks/useFetchUsers";
 //*Icons
 import salir from "/assets/back.svg";
