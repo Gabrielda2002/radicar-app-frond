b865893a24c91d49d01928f08a0be73d0927c1f8
Gabriel Duarte <duartearias2002@gmail.com>
2024-09-27 15:41:05 -0500

    adaptar reutilizar modal agregar dato

---
+++ src/services/createDataTableRadicacion.ts
@@ -0,0 +1,15 @@
+import { createTableRadicacion } from "../utils/api-config";
+
+export const createDataTableRadicacion = async (name: string, endPoint: string) => {
+    try {
+        
+        const response = await createTableRadicacion(name, endPoint);
+
+        if (response.status === 200) {
+            return response;
+        }
+
+    } catch (error) {
+        console.log(`Error al crear en la tabla ${endPoint}: ${error}`);
+    }
+}
--- src/utils/api-config.ts
+++ src/utils/api-config.ts
@@ -131,3 +131,7 @@ export const updateCupsAuditados = async (id: number, data: FormData) => {
 export const updateStatusData = async (id: number, status: string, endPoint: string) => {
     return api.put(`/${endPoint}/${id}`, { status });
 }
+
+export const createTableRadicacion = (name: string, endPoint: string) => {
+    return api.post(`/${endPoint}`, { name });
+}
