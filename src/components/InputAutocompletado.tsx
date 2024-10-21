e06ecebe5a8d6a010342e68665d8578b9dc65452
josting62 <jostingomez03@gmail.com>
2024-10-21 15:58:14 -0500

    se agrega funcionalidad para dar mensajes de errores y una alerta en el border de los inputs

--- src/components/InputAutocompletado.tsx
+++ src/components/InputAutocompletado.tsx
@@ -5,21 +5,20 @@ interface InputAutocompletadoProps {
   label: string;
   onInputChanged: (value: string, id?: string) => void;
   apiRoute: string;
+  error?: boolean; // Nueva propiedad para manejar el error
 }
 
 const InputAutocompletado: React.FC<InputAutocompletadoProps> = ({
   label,
   onInputChanged,
-  apiRoute
+  apiRoute,
+  error = false, // Establece un valor por defecto
 }) => {
   const [inputValue, setInputValue] = useState("");
-  {
-    /* hook  que trae los datos de  los inputs autocompletados */
-  }
   const {
     data,
-    error,
-    fetchInputAtcp
+    error: fetchError,
+    fetchInputAtcp,
   } = useFetchEspecialidadAtcp();
   const [showSuggestions, setShowSuggestions] = useState(false);
 
@@ -46,31 +45,33 @@ const InputAutocompletado: React.FC<InputAutocompletadoProps> = ({
   return (
     <div className="relative">
       <label>
-        <span className="block mb-2 font-bold text-gray-700 dark:text-gray-200">
+        <span className="flex mb-3 text-base font-bold text-gray-700 dark:text-gray-200">
           {label}
         </span>
         <input
           type="text"
           value={inputValue}
           onChange={handleInputChange}
-          className="w-full px-3 py-2 border border-gray-200 rounded dark:border-gray-600 text-stone-700 dark:text-white dark:bg-gray-800"
+          className={`w-full px-3 py-2 border-2 rounded dark:border-gray-600 text-stone-700 dark:text-white dark:bg-gray-800 ${
+            error ? "border-red-500 dark:border-red-500" : "border-gray-200"
+          }`}
           placeholder={`Escribe ${label.toLowerCase()}...`}
         />
       </label>
 
-        {error && <p>{error}</p>}
+      {fetchError && <p>{fetchError}</p>}
 
       {/* Mostrar sugerencias */}
       {showSuggestions && data && (
-        <ul className="absolute z-10 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-600 w-full rounded shadow-md mt-1 max-h-40 overflow-y-auto">
+        <ul className="absolute z-10 w-full mt-1 overflow-y-auto bg-white border border-gray-200 rounded shadow-md dark:bg-gray-800 dark:border-gray-600 max-h-40">
           {data.map((item) => (
-             <li
-             key={item.id}
-             className="px-3 py-2 text-gray-500"
-             onClick={() => handleSuggestionClick(item.name, item?.id)}
-         >
-             {item.name}
-         </li>
+            <li
+              key={item.id}
+              className="px-3 py-2 text-gray-500"
+              onClick={() => handleSuggestionClick(item.name, item?.id)}
+            >
+              {item.name}
+            </li>
           ))}
         </ul>
       )}
