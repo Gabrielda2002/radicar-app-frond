8399a197e2a9b2ca82cdc83dc3d7f2fb07dcf044
josting62 <jostingomez03@gmail.com>
2024-10-22 14:58:52 -0500

    se hacen cambios de estetica para mejorar el diseño

--- src/components/pages/modals/ModalSubirArchivo.tsx
+++ src/components/pages/modals/ModalSubirArchivo.tsx
@@ -73,7 +73,7 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
   return (
     <>
       {stadopen && (
-        <div className="fixed z-50 flex pt-16 justify-center transition-opacity duration-300 bg-black bg-opacity-40 -inset-5 backdrop-blur-sm">
+        <div className="fixed z-50 flex justify-center pt-16 transition-opacity duration-300 bg-black bg-opacity-40 -inset-5 backdrop-blur-sm">
           <section>
             <div
               className="fixed inset-0 transition-opacity duration-300 bg-black opacity-50 backdrop-blur-sm "
@@ -81,13 +81,13 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
             ></div>
 
             <div className="z-10 w-[800px] bg-white rounded overflow-hidden shadow-lg transform transition-transform duration-300 dark:bg-gray-800">
-              <div className="flex items-center justify-between  px-2 py-2 dark:bg-gray-800 ">
-                <h1 className="text-xl font-semibold text-color dark:text-gray-200 ">
+              <div className="flex items-center justify-between p-3 bg-gray-200 border-b-2 dark:bg-gray-600 border-b-gray-900 dark:border-b-white">
+                <h1 className="text-2xl font-semibold text-color dark:text-gray-200 ">
                   Subir Archivo
                 </h1>
                 <button
                   onClick={toggleModal}
-                  className="text-xl text-gray-500 hover-gray-700 pr-2"
+                  className="text-xl text-gray-400 duration-200 rounded-md dark:text-gray-100 w-7 h-7 hover:bg-gray-400 dark:hover:text-gray-900 hover:text-gray-900"
                 >
                   &times;
                 </button>
@@ -95,7 +95,7 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
 
               <div className="grid grid-cols-1 gap-10 mb-4">
                 <div className="p-4">
-                  <label className="block mb-2 font-bold text-gray-700 dark:text-gray-200">
+                  <label className="block mb-2 text-lg font-bold text-gray-700 dark:text-gray-200">
                     Archivos :
                   </label>
                   <input
@@ -107,7 +107,7 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
                     accept=".pdf,.doc,.docx,.xls,.xlsx"
                   />
                   <div
-                    className="flex items-center justify-between rounded cursor-pointer hover:bg-blue-400 active:bg-blue-500 dark:hover:bg-gray-600 dark:active:bg-gray-700 group  outline outline-1 outline-gray-300 "
+                    className="flex items-center justify-between duration-200 rounded cursor-pointer hover:bg-blue-400 active:bg-blue-500 dark:hover:bg-gray-600 dark:active:bg-gray-700 group outline outline-1 outline-gray-300 "
                     onClick={() => document.getElementById("files")?.click()}
                   >
                     <button className="px-2 py-4 text-sm font-semibold text-gray-800 dark:text-gray-300 hover:text-gray-200 ">
@@ -116,7 +116,7 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
                     <img
                       src={upload}
                       alt="Upload Icon"
-                      className="mr-4 w-8 h-8 mx-3 dark:invert group-hover:invert "
+                      className="w-8 h-8 mx-3 mr-4 dark:invert group-hover:invert "
                     />
                   </div>
 
@@ -143,15 +143,15 @@ const ModalSubirArchivo: React.FC<FileUploaderProps> = ({
                 </div>
               </div>
 
-              <div className="flex items-center justify-end w-full gap-2 px-4 py-4 text-sm font-semibold bg-white h-14 dark:bg-gray-800">
+              <div className="flex items-center justify-end w-full gap-2 px-4 py-4 text-sm font-semibold bg-gray-200 border-t-2 h-14 dark:bg-gray-600 border-t-gray-900 dark:border-t-white">
                 <button
                   onClick={toggleModal}
-                  className="w-20 h-10 text-blue-400 rounded-md hover:text-red-400 active:text-red-600 dark:text-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 dark:hover:text-gray-200"
+                  className="w-20 h-10 text-blue-400 duration-200 border-2 border-gray-400 rounded-md hover:border-red-500 hover:text-red-400 active:text-red-600 dark:text-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 dark:hover:text-gray-200"
                 >
                   Cerrar
                 </button>
                 <button
-                  className="w-20 h-10 text-white rounded-md bg-color hover:bg-emerald-900 active:bg-emerald-950 dark:bg-gray-900 dark:hover:bg-gray-600"
+                  className="w-20 h-10 text-white duration-200 border-2 rounded-md bg-color hover:bg-emerald-900 active:bg-emerald-950 dark:bg-gray-800 dark:hover:bg-gray-600"
                   disabled={uploading}
                   onClick={handleUpload}
                 >
