d998e9895800d4cd006dfce13b909636d52fa904
josting62 <jostingomez03@gmail.com>
2024-10-29 16:38:40 -0500

    se ajusta el modal para darle mejor sentido estetico

--- src/components/ModalSection.tsx
+++ src/components/ModalSection.tsx
@@ -18,26 +18,28 @@ const ModalSection: React.FC<ModuleSectionProps> = ({
     <section className="p-4 mb-6 bg-white rounded-md shadow-lg dark:bg-gray-800 shadow-indigo-500/40">
       <LoadingSpinner duration={duration} />
       <div className="flex items-center justify-between">
-        <h1 className="text-4xl font-bold text-color dark:text-gray-200">
-          {title}
-        </h1>
-        <nav>
-          <ol className="flex items-center space-x-2">
-            {breadcrumb.map((item, index) => (
-              <Link key={index} to={item.path}>
-                <li className="text-slate-400 hover:underline">{item.label}</li>
-              </Link>
-            ))}
-          </ol>
-        </nav>
-      </div>
-      <div className="mt-4">
-        <button
-          onClick={() => window.history.back()}
-          className="p-2 text-gray-600 duration-300 bg-gray-200 border-2 rounded-md hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:border-gray-700"
-        >
-          <ArrowUturnLeftIcon className="w-6 h-6" />
-        </button>
+        <div className="flex items-center">
+          <div className="">
+            <button
+              title="Regresa a la página anterior"
+              onClick={() => window.history.back()}
+              className="flex items-center p-2 text-gray-600 duration-300 bg-gray-200 border-2 rounded-md hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:border-gray-700"
+            >
+              <ArrowUturnLeftIcon className="w-6 h-6" />
+            </button>
+          </div>
+
+          <h1 className="pl-3 text-4xl font-bold text-color dark:text-gray-200">
+            {title}
+          </h1>
+        </div>
+        <ol className="flex items-center space-x-2">
+          {breadcrumb.map((item, index) => (
+            <Link key={index} to={item.path}>
+              <li className="text-slate-400 hover:underline">{item.label}</li>
+            </Link>
+          ))}
+        </ol>
       </div>
     </section>
   );
