b08a02a8b6bfcd4a3e31ea350ead0bde9fa3032a
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-08 08:07:58 -0500

    Agregando estilos
    Se agrega estilos componente padre y departamentos

--- src/components/pages/SistemaDeInventario/DepartamentosList.tsx
+++ src/components/pages/SistemaDeInventario/DepartamentosList.tsx
@@ -1,30 +1,80 @@
-import React from 'react'
-import { IDepartamentos } from '../../../models/IDepartamentos'
+import React from "react";
+import { IDepartamentos } from "../../../models/IDepartamentos";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+
+// imgs
+import Cucuta from "./Images/DEPART-cucuta.jpg";
+import Cundinamarca from "./Images/DEPART-cundinamarca.jpg";
+import Leticia from "./Images/DEPART-leticia.jpg";
 
 interface DepartamentosListProps {
-    departamentos: IDepartamentos[] | null;
-    onSelected: (departamentos: IDepartamentos) => void;
+  departamentos: IDepartamentos[] | null;
+  onSelected: (departamentos: IDepartamentos) => void;
 }
 
 const DepartamentosList: React.FC<DepartamentosListProps> = ({
-    departamentos,
-    onSelected
+  departamentos,
+  onSelected,
 }) => {
+  // imagen segun el departamento
+  const getIcon = (name: string) => {
+    switch (name) {
+      case "Norte de Santander":
+        return Cucuta;
+      case "Cundinamarca":
+        return Cundinamarca;
+      case "Amazonas":
+        return Leticia;
+      default:
+        return Cucuta;
+    }
+  };
+
   return (
     <>
-      <h2>
-        Departamentos
-      </h2>
       {departamentos?.map((d) => (
-        <button
-            key={d.id}
-            onClick={() => onSelected(d)}
-        >
-            {d.name}
-        </button>
+        <div className="relative w-full p-8 mx-auto overflow-hidden text-gray-900 duration-300 ease-in-out bg-gray-200 rounded-lg cursor-pointer dark:bg-gray-900 dark:text-gray-100 hover:-translate-y-1">
+          <button key={d.id} onClick={() => onSelected(d)}>
+            <div className="absolute top-0 left-0 w-56 h-full">
+              {/* Imagen de fondo */}
+              <img
+                src={getIcon(d.name)}
+                alt=""
+                className="absolute top-0 left-0 w-56 -z-0 opacity-30"
+              />
+              <div className="absolute top-0 right-0 w-1/2 h-full bg-gradient-to-r from-transparent to-gray-200 dark:to-gray-900"></div>
+            </div>
+
+            {/* Contenido principal con z-index positivo */}
+            <div className="relative">
+              <div className="flex justify-between space-x-8">
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center w-12 h-12 bg-gray-500 rounded-full dark:bg-teal-600">
+                    <MapPinIcon className="text-gray-100 dark:text-gray-100 w-9 h-9" />
+                  </div>
+                  <div>
+                    <h1 className="pl-3 text-3xl font-semibold">{d.name}</h1>
+                  </div>
+                </div>
+
+                {/* Numero de sedes */}
+                {/* <div className="flex items-center">
+                  <button className="flex p-2 text-sm text-white bg-gray-700 rounded-full dark:bg-teal-600">
+                    <div className="flex items-center">
+                      <h2 title="Click para ver las categorias completas">
+                        6 Sedes
+                      </h2>
+                    </div>
+                  </button>
+                </div> */}
+              </div>
+              <div></div>
+            </div>
+          </button>
+        </div>
       ))}
     </>
-  )
-}
+  );
+};
 
-export default DepartamentosList
+export default DepartamentosList;
--- src/components/pages/SistemaDeInventario/SistemaInventario.tsx
+++ src/components/pages/SistemaDeInventario/SistemaInventario.tsx
@@ -5,6 +5,7 @@ import useFetchSedes from "../../../hooks/useFetchSedes";
 import SedesList from "./sedesList";
 import useFetchItems from "../../../hooks/useFetchItems";
 import ItemsList from "./ItemsList";
+import ModalSection from "../../ModalSection";
 
 const SistemaInventario: React.FC = () => {
   const {
@@ -16,94 +17,99 @@ const SistemaInventario: React.FC = () => {
   const { sedes } = useFetchSedes(departmentSelect);
   const [sedeSelect, setSedeSelect] = useState<number | null>(null);
 
-  const [ tipoItem, setTipoItem ] = useState<'equipos' | 'dispositivos-red' | null>(null);
+  const [tipoItem, setTipoItem] = useState<
+    "equipos" | "dispositivos-red" | null
+  >(null);
   const { items } = useFetchItems(sedeSelect, tipoItem);
 
   // estado para manejar la pantalla actual
-  const [screen, setScreen] = useState<"departamentos" | "sedes" | "tipoItem" | "items">(
-    "departamentos"
-  );
+  const [screen, setScreen] = useState<
+    "departamentos" | "sedes" | "tipoItem" | "items"
+  >("departamentos");
 
   // funcion ir al componente anterior
   const handleBack = () => {
     if (screen === "items") {
       setScreen("tipoItem");
       setTipoItem(null);
-    }else if (screen === "tipoItem") {
+    } else if (screen === "tipoItem") {
       setScreen("sedes");
       setSedeSelect(null);
-    }else if (screen === "sedes") {
+    } else if (screen === "sedes") {
       setScreen("departamentos");
       setDepartmentSelect(null);
     }
-    
-   
   };
 
   return (
+    <div>
+      <ModalSection
+        title="Módulo Inventario"
+        breadcrumb={[
+          { label: "Inicio", path: "/home" },
+          { label: "/ Sistema De Inventario", path: "" },
+        ]}
+      />
 
-      <div>
+      <div className="w-full p-5 ml-0 bg-white rounded-md shadow-lg dark:bg-gray-800 mb-11 shadow-indigo-500/40">
         {/* si scree en diferente a departamentos se muestra la flecha */}
-      {screen !== 'departamentos' && (
-        <button
-          onClick={handleBack}
-          className="btn btn-secondary"
-        >
-          Atras
-        </button>
-      )}
+        {screen !== "departamentos" && (
+          <button onClick={handleBack} className="btn btn-secondary">
+            Atras
+          </button>
+        )}
 
-      {screen === 'departamentos' && (
-        loadingDepartment ? (
-          <p>Cargando departamentos...</p>
-        ) : errordepartment ? (
-          <p>{errordepartment}</p>
-        ) : (
-          <DepartamentosList
-            departamentos={departments}
-            onSelected={(departamento) => {
-              setDepartmentSelect(departamento.id);
-              setScreen('sedes'); // cambia la pantalla a sedes
+        {screen === "departamentos" &&
+          (loadingDepartment ? (
+            <p>Cargando departamentos...</p>
+          ) : errordepartment ? (
+            <p>{errordepartment}</p>
+          ) : (
+            <DepartamentosList
+              departamentos={departments}
+              onSelected={(departamento) => {
+                setDepartmentSelect(departamento.id);
+                setScreen("sedes"); // cambia la pantalla a sedes
+              }}
+            />
+          ))}
+
+        {screen === "sedes" && departmentSelect && (
+          <SedesList
+            sedes={sedes}
+            onSelect={(sede) => {
+              setSedeSelect(sede.id);
+              setScreen("tipoItem"); // cambia la pantalla a items
             }}
           />
-        )
-      )}
+        )}
 
-      { screen === 'sedes' && departmentSelect && (
-        <SedesList
-          sedes={sedes}
-          onSelect={(sede) => {
-            setSedeSelect(sede.id);
-            setScreen('tipoItem'); // cambia la pantalla a items
-          }}
-        />
-      )}
+        {screen === "tipoItem" && sedeSelect && (
+          <div>
+            <h2>Selecciona el tipo de item</h2>
+            <button
+              onClick={() => {
+                setTipoItem("equipos");
+                setScreen("items");
+              }}
+            >
+              Computadores
+            </button>
+            <button
+              onClick={() => {
+                setTipoItem("dispositivos-red");
+                setScreen("items");
+              }}
+            >
+              Dispositivos de red
+            </button>
+          </div>
+        )}
 
-      {screen === 'tipoItem' && sedeSelect && (
-        <div>
-          <h2>Selecciona el tipo de item</h2>
-          <button
-            onClick={() => {
-              setTipoItem('equipos');
-              setScreen('items');
-            }}
-          >
-            Computadores
-          </button>
-          <button
-            onClick={() => {
-              setTipoItem('dispositivos-red');
-              setScreen('items');
-            }}
-          >
-            Dispositivos de red
-          </button>
-        </div>
-      )}
-
-      { screen === 'items' && sedeSelect && tipoItem && (
-        <ItemsList invetario={items} tipoItem={tipoItem} />
-      )}
+        {screen === "items" && sedeSelect && tipoItem && (
+          <ItemsList invetario={items} tipoItem={tipoItem} />
+        )}
+      </div>
     </div>
   );
 };
