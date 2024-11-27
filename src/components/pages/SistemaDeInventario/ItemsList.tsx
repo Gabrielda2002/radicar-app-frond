a1b84285eb24b58a96e010f99e3092b8799dcdc4
josting62 <jostingomez03@gmail.com>
2024-11-27 14:16:58 -0500

    "Style" "Fix" "Remove" Ajuste de cookie para guardar el estado del ItemList.
    -Se elimina la cookie que guarda el estado del tipo de vista, ya que interfiere con los modales y su presentación

--- src/components/pages/SistemaDeInventario/ItemsList.tsx
+++ src/components/pages/SistemaDeInventario/ItemsList.tsx
@@ -1,6 +1,4 @@
-// * Fuctions and Hooks
 import React, { useEffect, useState } from "react";
-import Cookies from "js-cookie";
 import { IItems } from "../../../models/IItems";
 import LoadingSpinner from "../../LoadingSpinner";
 import ModalItemsForm from "../modals/ModalItemsForm";
@@ -9,13 +7,12 @@ import ModalAccesorioItem from "../modals/ModalAccesorioItem";
 import { IItemsNetworking } from "../../../models/IItemsNetworking";
 import ModalTablaseguimientoItem from "../modals/ModalTablaSeguimientoItem";
 
-// * Interface
 interface ItemsListProps {
   invetario: IItems[] | IItemsNetworking[] | null;
   tipoItem: "equipos" | "dispositivos-red" | null;
   idSede: number | null;
 }
-// * Icons
+
 import {
   ListBulletIcon,
   Squares2X2Icon,
@@ -32,31 +29,12 @@ const ItemsList: React.FC<ItemsListProps> = ({
     null
   );
   const [isLoading, setIsLoading] = useState(true);
-  const [isGridView, setIsGridView] = useState(true); // * Estado para alternar vistas (Lista o de cuadrícula)
-
-  // * Efecto para almacenar una cookie para el estado de la vista
-
-  useEffect(() => {
-    const savedView = Cookies.get("itemsViewMode");
-    if (savedView === "list") {
-      setIsGridView(false);
-    }
-  }, []);
-
-  // * Efecto para guardar el estado de la vista
-
-  const toggleViewMode = () => {
-    const newViewMode = !isGridView ? "grid" : "list";
-    Cookies.set("itemsViewMode", newViewMode, { expires: 7 });
-    setIsGridView(!isGridView);
-  };
-
-  // * Efecto para determinar el cierre del modal o apertura del mismo
+  const [isGridView, setIsGridView] = useState(true); // * Estado para alternar vistas
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsLoading(false);
-    }, 1000); // Simular carga inicial
+    }, 1000); // * Simular carga inicial
     return () => clearTimeout(timeout);
   }, [tipoItem]);
 
@@ -68,9 +46,8 @@ const ItemsList: React.FC<ItemsListProps> = ({
     setSelected(null);
   };
 
-  // * Funcion para mostrar el tipo de icon (en modo de lista)
-  // * Iconos que, dependediendo del Item de Inventario, se muestre el icono del Dispositivo
-
+   // 
+  // * Iconos que, dependediendo del Item de Inventario, se muestre el icono de Inventario
   const getIcon = () => {
     if (tipoItem === "equipos") {
       return <ComputerDesktopIcon className="w-8 h-8 mr-2 dark:text-white" />;
@@ -106,22 +83,10 @@ const ItemsList: React.FC<ItemsListProps> = ({
               type="text"
               className="w-full p-2 border-2 rounded-md dark:border-gray-600 dark:bg-gray-800 dark:text-white"
               placeholder="Busqueda de Dispositivo..."
-              // value={query}
-              // onChange={(e) =>setQuery(e.target.value)}
             />
-            <select
-              name=""
-              id=""
-              className="border-2 ml-2 border-stone-300 h-[45px] w-[100px] rounded-md  focus:outline-none text-stone-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-            >
-              <option value="">Paginas</option>
-              <option value="">10 items</option>
-              <option value="">20 items</option>
-              <option value="">30 items</option>
-            </select>
             <button
-              className="flex items-center px-4 py-1 ml-2 transition-colors duration-300 bg-gray-200 rounded-md text-pretty hover:text-white hover:bg-gray-700 dark:text-white dark:bg-color dark:hover:bg-teal-600"
-              onClick={toggleViewMode} // * Alternar vista
+              className="flex items-center px-4 py-1 ml-4 transition-colors duration-300 bg-gray-200 rounded-md text-pretty hover:text-white hover:bg-gray-700 dark:text-white dark:bg-color dark:hover:bg-teal-600"
+              onClick={() => setIsGridView(!isGridView)} // Alternar vista
             >
               {isGridView ? (
                 <>
@@ -138,14 +103,13 @@ const ItemsList: React.FC<ItemsListProps> = ({
           </div>
 
           {/* Renderizado Condicional */}
-          {/* {filteredData.length > 0 ? ()} */}
           {invetario && invetario.length > 0 ? (
             isGridView ? (
               <div className="grid gap-6 transition-all duration-500 ease-in-out md:grid-cols-2 lg:grid-cols-3">
                 {invetario.map((item) => (
                   <div
                     key={item.id}
-                    className="relative p-4 duration-500 border rounded-md shadow-sm dark:shadow-indigo-600 hover:shadow-lg dark:hover:shadow-indigo-600 dark:border-gray-700 hover:-translate-y-1"
+                    className="relative p-4 duration-500 border rounded-md shadow-md dark:shadow-md dark:shadow-indigo-600 hover:shadow-xl dark:hover:shadow-indigo-950 dark:border-gray-700"
                   >
                     <div className="flex items-center justify-between mb-14">
                       <h3 className="mb-2 text-2xl font-semibold dark:text-white">
@@ -191,7 +155,7 @@ const ItemsList: React.FC<ItemsListProps> = ({
                 {invetario.map((item) => (
                   <div
                     key={item.id}
-                    className="flex items-center justify-between p-4 duration-500 border rounded-md shadow-sm dark:border-gray-700 dark:shadow-indigo-600 hover:shadow-xl dark:hover:shadow-indigo-600 hover:-translate-y-2"
+                    className="flex items-center justify-between p-4 duration-500 border rounded-md shadow-md dark:border-gray-700 dark:shadow-md dark:shadow-indigo-600 hover:shadow-xl dark:hover:shadow-indigo-950"
                   >
                     <div>
                       <div className="flex items-center">
