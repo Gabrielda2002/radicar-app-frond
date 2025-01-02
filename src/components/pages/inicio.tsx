5361dc2d022da32d524a8213c1e3c9e92779b4b0
DuvalG25 <Duvalgalvis25@gmail.com>
2025-01-02 14:24:14 -0500

    Style: ajuste en la estructura de 'Consultar Servicios Contradados' en la pagina de Inicio

--- src/components/pages/inicio.tsx
+++ src/components/pages/inicio.tsx
@@ -1,7 +1,7 @@
 //*Fuctions and Hooks
 import LoadingSpinner from "../LoadingSpinner";
 import { useEffect, useState, lazy, Suspense } from "react";
-import {AnimatePresence} from 'framer-motion'
+import { AnimatePresence } from "framer-motion";
 
 // TODO: Importar componentes y hooks necesarios
 //*Icons
@@ -19,8 +19,7 @@ const Inicio = () => {
   const [codigo, setCodigo] = useState("");
 
   // hook consultar servicios
-  const { servicios, errorServicios, getData } =
-    useFetchServicioContratado();
+  const { servicios, errorServicios, getData } = useFetchServicioContratado();
 
   useEffect(() => {
     const alertClosed = localStorage.getItem("alertClosed");
@@ -70,75 +69,108 @@ const Inicio = () => {
             </div>
             {/* Sección de bienvenida */}
             <div className="flex flex-col w-full pb-10 mb-10 border-2 rounded-lg shadow-md dark:border-color bg-gray-50 dark:bg-gray-700 dark:shadow-indigo-800">
-              <div className="p-4">
-                <h2 className="pb-6 pl-10 mt-4 text-5xl font-bold dark:text-white">
+              <div className="p-6">
+                <h2 className="pb-2 pl-2 mt-2 text-5xl font-bold dark:text-white">
                   Consultar Servicios Contratados:
                 </h2>
-                <div className="grid grid-cols-2 gap-2">
-                  <label htmlFor="">
-                    Ingrese codigo del servicio:
-                    <input
-                      type="text"
-                      value={codigo}
-                      onChange={(e) => setCodigo(e.target.value)}
-                      className="border-2 border-gray-300 p-2 w-full rounded-lg"
-                      placeholder="Codigo del servicio"
-                    />
+                <div className="p-2 m-5 rounded-lg ">
+                  <label htmlFor="" className="w-5/6 dark:text-gray-200 text-[24px]">
+                    Ingrese el código del servicio:
+                    <div>
+                      <input
+                        type="text"
+                        value={codigo}
+                        onChange={(e) => setCodigo(e.target.value)}
+                        className="p-2 mt-3 mr-4 border-2 border-gray-400 rounded-lg b-2 w-60 dark:text-gray-700 text-[17px]"
+                        placeholder="Código del servicio"
+                      />
                     <button
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                      className="px-5 font-bold text-white bg-teal-500 rounded-md shadow-md mt-23 py-2 hover:bg-teal-600 hover:shadow-teal-600 dark:hover:shadow-teal-500 text-[16px]"
                       onClick={() => getData(codigo)}
-                    >
+                      >
                       Consultar
                     </button>
-                    {servicios?.map((servicio) => (
-                      <table>
-                        <thead>
-                          <tr>
-                            <td>Codigo</td>
-                            <td>Descripcion Servicio</td>
-                            <td></td>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>{servicio.code}</td>
-                            <td>{servicio.description}</td>
-                            <table>
-                              <thead>
-                                <tr>
-                                  <td>Eps</td>
-                                  <td>Sede</td>
-                                  <td>Estado</td>
-                                </tr>
-                              </thead>
-                              {servicio.Relations.map((r) => (
-                                <tbody>
-                                  <tr>
-                                    <td>{r.nameConvenio}</td>
-                                    <td>{r.nameSede}</td>
-                                    <td>
-                                      {r.isContrated
-                                        ? "Contratado"
-                                        : "No Contratado"}
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              ))}
-                            </table>
-                          </tr>
-                        </tbody>
-                      </table>
-                    ))}
-                    <AnimatePresence>
-                      {
-                        errorServicios && (
-                          <div className="text-red-500">
-                            {errorServicios}
-                          </div>
-                        )
-                      }
-                    </AnimatePresence>
+                      </div>
                   </label>
+                  <div className="flex flex-col w-full mt-4 justify-items-center dark:text-gray-200">
+                    {servicios?.map((servicio) => (
+                      <div  
+                        key={servicio.code}
+                        className="overflow-x-auto border rounded-lg"
+                      >
+                        <table className="w-full table-auto">
+                          <thead>
+                            <tr className="text-gray-700 bg-gray-400 dark:bg-gray-800 dark:text-gray-200">
+                              <th className="px-2 py-3 text-center whitespace-nowrap">
+                                Código
+                              </th>
+                              <th className="px-2 py-3 text-center">
+                                Descripción Servicio
+                              </th>
+                              <th className="px-3 py-3 text-center whitespace-nowrap">
+                                Sede
+                              </th>
+                              <th className="py-3 text-center px-14 whitespace-nowrap">
+                                Eps
+                              </th>
+                              <th className="px-6 py-3 text-center whitespace-nowrap">
+                                Estado
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr className="bg-gray-200 border-teal-700 dark:bg-gray-700">
+                              <td className="px-6 py-3 font-medium text-center">
+                                {servicio.code}
+                              </td>
+                              <td className="px-1 py-3 text-sm text-center">
+                                {servicio.description}
+                              </td>
+                              <td colSpan={3} className="px-2 py-3">
+                                <table className="w-full">
+                                  <tbody>
+                                    {servicio.Relations.map((r, index) => (
+                                      <tr
+                                        key={`${r.nameConvenio}-${r.nameSede}-${index}`}
+                                        className="border-b border-gray-400 dark:border-gray-400"
+                                      >
+                                        <td className="px-2 py-2 text-center whitespace-nowrap">
+                                          {r.nameConvenio}
+                                        </td>
+                                        <td className="px-2 py-2 text-center whitespace-nowrap">
+                                          {r.nameSede}
+                                        </td>
+                                        <td className="px-2 py-2 text-center">
+                                          <span
+                                            className={`inline-flex rounded-full px-3
+                                               py-1 text-xs font-semibold ${
+                                              r.isContrated
+                                                ? "bg-teal-500 text-gray-800 dark:bg-teal-600 dark:text-white"
+                                                : "bg-red-500 text-gray-800 dark:bg-red-600 dark:text-white"
+                                            }`}
+                                          >
+                                            {r.isContrated
+                                              ? "Contratado"
+                                              : "No Contratado"}
+                                          </span>
+                                        </td>
+                                      </tr>
+                                    ))}
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    ))}
+                  </div>
+
+                  <AnimatePresence>
+                    {errorServicios && (
+                      <div className="text-red-500">{errorServicios}</div>
+                    )}
+                  </AnimatePresence>
                 </div>
               </div>
 
