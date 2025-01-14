7a43c0e88a9d6ce65234c23c862c4d2ebb9601cc
Gabriel Duarte <duartearias2002@gmail.com>
2025-01-14 15:50:36 -0500

    Agrega campos, footer formulario solicitar carta

--- src/featuures/RecoverLetter/Components/ModalRequestForm.tsx
+++ src/featuures/RecoverLetter/Components/ModalRequestForm.tsx
@@ -16,7 +16,7 @@ const ModalRequestForm: FC<ModalRequestFormProps> = ({ isOpen, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex justify-center w-full h-full pt-16 duration-300 bg-black w ransition-opacity bg-opacity-40 backdrop-blur-sm">
       <section
-        className={`z-10 w-[600px] h-[600px] bg-white rounded overflow-hidden shadow-lg transform transition-transform duration-300 dark:bg-gray-800 ${
+        className={`z-10 w-[600px] h-fit bg-white rounded overflow-hidden shadow-lg transform transition-transform duration-300 dark:bg-gray-800 ${
           showAnimation && !closing
             ? "translate-y-0 opacity-100"
             : "translate-y-10 opacity-0"
@@ -34,8 +34,51 @@ const ModalRequestForm: FC<ModalRequestFormProps> = ({ isOpen, onClose }) => {
           </button>
         </div>
 
-        
-        
+        <form className="dark:bg-gray-800">
+          <div className="px-5 max-h-[70vh] overflow-y-auto">
+            <div>
+              <h5 className="p-2 mt-2 text-xl font-semibold text-blue-500 dark:text-gray-200">
+                Datos para hacer la solicitud
+              </h5>
+            </div>
+
+            <section className="grid-cols-1 mb-6 text-sm gap-x-10 gap-y-2 ms-2">
+                <div>
+                    <label htmlFor="justify">
+                        <span className="flex items-center text-base font-bold text-gray-700 after:content-['*'] after:ml-2 after:text-red-600 dark:text-gray-200">
+                            Justificacion
+                        </span>
+                        <input 
+                            type="text"
+                            id="justify"
+                            name="justify"
+                            placeholder="Escriba"
+                            className="w-full px-3 py-2 text-sm border border-gray-200 rounded dark:border-gray-600 text-stone-700 dark:text-white dark:bg-gray-800"
+                        />
+                    </label>
+                </div>
+            </section>
+
+
+          </div>
+            <div
+                className="flex items-center justify-end w-full gap-2 px-4 py-4 text-sm font-semibold bg-gray-300 border-t-2 h-14 dark:bg-gray-600 border-t-gray-900 dark:border-t-white"
+            >
+                <button
+                type="button"
+                    onClick={onClose}
+                    className="w-20 h-10 text-blue-400 duration-200 border-2 border-gray-500 rounded-md hover:border-red-500 hover:text-red-500 active:hover:bg-gray-600 dark:hover:text-gray-200 dark:text-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600"
+                >
+                    Cerrar
+                </button>
+                <button
+                    type="submit"
+                    className="w-20 h-10 text-white duration-200 border-2 rounded-md dark:hover:border-gray-900 bg-color hover:bg-emerald-900 active:bg-emerald-950 dark:bg-gray-800 dark:hover:bg-gray-600 "
+                >
+                    Solicitar
+                </button>
+            </div>
+        </form>
       </section>
     </div>
   );
