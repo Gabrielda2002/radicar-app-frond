9bf922c991d4300974229dcab53890619d74deb9
DuvalG25 <Duvalgalvis25@gmail.com>
2025-01-22 15:12:51 -0500

    style: mejora el estilo del botón de búsqueda en TablaRadicacion

--- src/featuures/Radicacion/Page/TablaRadicacion.tsx
+++ src/featuures/Radicacion/Page/TablaRadicacion.tsx
@@ -115,7 +115,7 @@ const TablaRadicacion = () => {
               className="w-64 h-10 pl-3 border rounded-md border-stone-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
             />
             <button
-              className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md"
+              className="w-full text-white bg-teal-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:translate-y-1 hover:scale-100 hover:bg-emerald-700 duration-300 mt-3"
               onClick={() => getData(documento)}
             >
               Buscar
