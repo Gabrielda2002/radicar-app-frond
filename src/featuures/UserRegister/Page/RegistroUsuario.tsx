673d521e402b321c1b70c4d38dcada97c874edd7
Gabriel Duarte <duartearias2002@gmail.com>
2025-03-27 09:42:28 -0500

    Feat: barra de búsqueda y paginación registros usuarios

--- src/featuures/UserRegister/Page/RegistroUsuario.tsx
+++ src/featuures/UserRegister/Page/RegistroUsuario.tsx
@@ -1,70 +1,40 @@
-import { useState, useMemo } from "react";
+import { useCallback, useState } from "react";
 import ModalSection from "@/components/common/HeaderPage/HeaderPage.tsx";
 import { useFetchRegisterEntries } from "../Hooks/useFetchRegisterEntries";
 import { FormatDate } from "@/utils/FormatDate";
 import Pagination from "@/components/common/PaginationTable/PaginationTable";
+import useSearch from "@/hooks/useSearch.ts";
+import { IRegisterUser } from "@/models/IRegisterUser.ts";
+import usePagination from "@/hooks/usePagination";
 
 const RegistroUsuario = () => {
+  const ITEMS_PER_PAGE = 8;
+  const [itemsPerPage] = useState(ITEMS_PER_PAGE);
   const [documento, setDocumento] = useState<string>("");
   const [dateStart, setDateStart] = useState<string>("");
   const [dateEnd, setDateEnd] = useState<string>("");
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
-  const [query, setQuery] = useState<string>("");
 
   const { dataRegister, getData, errorRegister } = useFetchRegisterEntries();
 
-  // funcion de filtro y busqueda
-  const filteredAndSearchedData = useMemo(() => {
-    let result = dataRegister;
-
-
-    if (documento) {
-      result = result.filter((user) =>
-        user.documentNumber.toString().includes(documento)
-      );
-    }
-
-    if (dateStart && dateEnd) {
-      result = result.filter((user) => {
-        const userDate = new Date(user.registerDate);
-        const startDate = new Date(dateStart);
-        const endDate = new Date(dateEnd);
-        return userDate >= startDate && userDate <= endDate;
-      });
-    }
-
-    if (query) {
-      const lowerQuery = query.toLowerCase().trim();
-      result = result.filter(
-        (user) =>
-          user.documentNumber.toString().toLowerCase().includes(lowerQuery) ||
-          user.userName.toLowerCase().includes(lowerQuery) ||
-          user.userLastName.toLowerCase().includes(lowerQuery) ||
-          user.headquarters.toLowerCase().includes(lowerQuery) ||
-          user.hourRegister.toLowerCase().includes(lowerQuery)
-      );
-    }
-
-    return result;
-  }, [dataRegister, documento, dateStart, dateEnd, query]);
-
-  // Logica de paginacion
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = filteredAndSearchedData.slice(
-    indexOfFirstItem,
-    indexOfLastItem
+  const { query, setQuery, filteredData } = useSearch<IRegisterUser>(
+    dataRegister,
+    [
+      "documentNumber",
+      'headquarters',
+      'hourRegister',
+      'registerDate'
+    ]
   );
 
-  const paginate = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
+  const { currentPage, totalPages, paginate, currentData, setItemsPerPage } =
+    usePagination(filteredData, ITEMS_PER_PAGE);
 
-  const handleFilterChange = () => {
-    setCurrentPage(1);
-    getData(documento, dateStart, dateEnd);
-  };
+  const handleItemsPerPageChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setItemsPerPage(Number(e.target.value));
+    },
+    [setItemsPerPage]
+  );
 
   return (
     <>
@@ -112,7 +82,7 @@ const RegistroUsuario = () => {
             />
             <button
               className="w-full text-white bg-teal-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:translate-y-1 hover:scale-100 hover:bg-emerald-700 duration-300 mt-3"
-              onClick={handleFilterChange}
+              onClick={() => getData(documento, dateStart, dateEnd)}
             >
               Buscar
             </button>
@@ -120,34 +90,28 @@ const RegistroUsuario = () => {
         </section>
 
         {/* vista escritorio */}
-        {currentItems.length > 0 && (
+        <div className="flex flex-col items-center justify-between mb-4 md:flex-row">
+          <div className="flex items-center w-full space-x-2">
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Buscar..."
+              className="block ps-2 w-full h-10 pl-1 border-[1px] border-stone-300 text-stone-700 rounded-md bg-blue-50 focus:outline-none focus:ring-2 focus:bg-blue-100 dark:focus:bg-gray-500 dark:focus:ring-gray-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+            />
+            <select
+              value={itemsPerPage}
+              onChange={handleItemsPerPageChange}
+              className="w-full h-10 border border-gray-300 rounded-md md:w-24 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+            >
+              <option value="10">10 Páginas</option>
+              <option value="20">20 Páginas</option>
+              <option value="30">30 Páginas</option>
+            </select>
+          </div>
+        </div>
+        {filteredData.length > 0 ? (
           <>
             {/* Paginatcion y busqueda */}
-            <div className="flex flex-col items-center justify-between mb-4 md:flex-row">
-              <div className="flex items-center w-full space-x-2">
-                <input
-                  value={query}
-                  onChange={(e) => {
-                    setQuery(e.target.value);
-                    setCurrentPage(1);
-                  }}
-                  placeholder="Buscar..."
-                  className="block ps-2 w-full h-10 pl-1 border-[1px] border-stone-300 text-stone-700 rounded-md bg-blue-50 focus:outline-none focus:ring-2 focus:bg-blue-100 dark:focus:bg-gray-500 dark:focus:ring-gray-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                />
-                <select
-                  value={itemsPerPage}
-                  onChange={(e) => {
-                    setItemsPerPage(Number(e.target.value));
-                    setCurrentPage(1);
-                  }}
-                  className="w-full h-10 border border-gray-300 rounded-md md:w-24 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                >
-                  <option value="10">10 Páginas</option>
-                  <option value="20">20 Páginas</option>
-                  <option value="30">30 Páginas</option>
-                </select>
-              </div>
-            </div>
 
             <table className="hidden w-full text-sm text-left text-gray-500 md:table rtl:text-right dark:text-gray-200">
               <thead className="text-xs text-gray-800 uppercase bg-gray-300 shadow-xl huver: dark:bg-gray-600 dark:text-gray-200">
@@ -161,7 +125,7 @@ const RegistroUsuario = () => {
                 </tr>
               </thead>
               <tbody>
-                {currentItems.map((u) => (
+                {currentData().map((u) => (
                   <tr
                     key={u.id}
                     className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
@@ -182,13 +146,17 @@ const RegistroUsuario = () => {
             <div className="justify-center hidden mt-4 md:flex">
               <Pagination
                 currentPage={currentPage}
-                totalPages={Math.ceil(
-                  filteredAndSearchedData.length / itemsPerPage
-                )}
+                totalPages={totalPages}
                 onPageChange={paginate}
               />
             </div>
           </>
+        ): (
+          dataRegister.length > 0 && (
+            <div className="p-4 text-center text-red-500 dark:text-red-300">
+              No se encontraron resultados que coincidan con la búsqueda.
+            </div>
+          )
         )}
         {errorRegister && (
           <div className="text-red-500">
@@ -198,9 +166,9 @@ const RegistroUsuario = () => {
 
         {/* responsive*/}
         <div className="grid w-full grid-cols-1 gap-4 md:hidden">
-          {currentItems.map((usuario) => (
+          {currentData().map((usuario) => (
             <div
-              key={usuario.documentNumber}
+              key={usuario.id}
               className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600"
             >
               <div className="grid grid-cols-[40%_60%] gap-2 text-sm">
@@ -252,9 +220,7 @@ const RegistroUsuario = () => {
           <div className="justify-center block mb-4 place-self-center w-80">
             <Pagination
               currentPage={currentPage}
-              totalPages={Math.ceil(
-                filteredAndSearchedData.length / itemsPerPage
-              )}
+              totalPages={totalPages}
               onPageChange={paginate}
             />
           </div>
