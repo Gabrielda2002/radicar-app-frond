8659b7875a8f3668e1b56374c74d9d3cc01d5e1a
Gabriel Duarte <duartearias2002@gmail.com>
2025-03-28 14:11:37 -0500

    Refactor: mejorar la carga de componentes y ajustar la estructura de rutas en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { lazy } from "react";
 //*Componentes
 import Footer from "./components/layout/footer/page/Footer.tsx";
@@ -8,18 +7,19 @@ import Login from "./featuures/auth/Page/login.tsx";
 import SideBar from "./components/layout/sidebar/page/SideBar.tsx";
 import { Route, Routes, Navigate } from "react-router-dom";
 import ContextualizedRoutes from "./components/Routes/ContextualizedRoutes.tsx";
-const CookieConsent = lazy(() => import("./components/common/CookieConsent/PopCookie.tsx"));
-
+const CookieConsent = lazy(
+  () => import("./components/common/CookieConsent/PopCookie.tsx")
+);
 
 //*Contextos
-import "react-toastify/dist/ReactToastify.css"; 
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/authContext";
 import { useTheme } from "./context/blackWhiteContext";
 import { Bounce, ToastContainer } from "react-toastify";
 import { SidebarProvider } from "./context/sidebarContext";
 import { PrivateRoutes } from "./components/PrivateRoutes";
 import { UserProfileProvider } from "./context/userProfileContext";
-import {NotificationProvider} from "@/context/notificationContext.tsx";
+import { NotificationProvider } from "@/context/notificationContext.tsx";
 import { TicketProvider } from "./context/ticketContext.tsx";
 
 function AppRoutes() {
@@ -41,66 +41,6 @@ function AppRoutes() {
                   <main className="flex-1 overflow-auto bg-slate-200 dark:bg-gray-900">
                     <Layout>
                       <ContextualizedRoutes />
-                      {/* <Suspense
-                        fallback={
-                          <div className="flex items-center justify-center">
-                            <LoadingSpinner />
-                          </div>
-                        }
-                      >
-                        <Routes>
-                          <Route path="/home" element={<Inicio />} />
-                          <Route
-                            path="/tabla-radicacion"
-                            element={<TablaRadicacion />}
-                          />
-                          <Route
-                            path="/tabla-auditoria"
-                            element={<TablaAuditoria />}
-                          />
-                          <Route
-                            path="/tabla-cirugias"
-                            element={<TablaCirugias />}
-                          />
-                          <Route path="/perfil" element={<Perfil />} />
-                          <Route
-                            path="/registrar-usuarios"
-                            element={<RegistrarUsuarios />}
-                          />
-                          <Route path="/usuarios" element={<Usuarios />} />
-                          <Route
-                            path="/tabla-autorizar-servicios"
-                            element={<TablaAutorizarServicios />}
-                          />
-                          <Route
-                            path="/SistemGestionCalidad"
-                            element={<FileManager />}
-                          />
-                          <Route
-                            path="/carta-recobro"
-                            element={<RecoverLetterPage />}
-                          />
-                          <Route
-                            path="/auditoria"
-                            element={<RecoverLastPage />}
-                          />
-                          {/* rutas de las tablas radicacion */}
-                          {/* <Route path="*" element={<TableRoutes />} />
-                          <Route
-                            path="/SistemaInventario"
-                            element={<SistemaInventario />}
-                          />
-                          <Route path="*" element={<Navigate to="/home" />} />
-                          <Route
-                            path="/RegistroUsuarios"
-                            element={<RegistroUsuarios />}
-                          />
-                          <Route
-                            path="/GestionTickets"
-                            element={<GestionTickets />}
-                          />
-                        </Routes>
-                      </Suspense> */} 
                     </Layout>
                   </main>
                 </div>
@@ -124,25 +64,25 @@ export function App() {
       <AuthProvider>
         <UserProfileProvider>
           <NotificationProvider>
-          <TicketProvider>
-          <SidebarProvider>
-            <CookieConsent />
-            <AppRoutes />
-            <ToastContainer
-              position="bottom-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-              transition={Bounce}
-            />
-          </SidebarProvider>
-          </TicketProvider>
+            <TicketProvider>
+              <SidebarProvider>
+                <CookieConsent />
+                <AppRoutes />
+                <ToastContainer
+                  position="bottom-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="colored"
+                  transition={Bounce}
+                />
+              </SidebarProvider>
+            </TicketProvider>
           </NotificationProvider>
         </UserProfileProvider>
       </AuthProvider>
--- src/components/Routes/ContextualizedRoutes.tsx
+++ src/components/Routes/ContextualizedRoutes.tsx
@@ -7,21 +7,42 @@ import LoadingSpinner from "@/components/common/LoadingSpinner/LoadingSpinner.ts
 //*Lazy Components
 const Inicio = lazy(() => import("@/featuures/home/page/Inicio.tsx"));
 const Perfil = lazy(() => import("@/featuures/profile/page/PerfilPage.tsx"));
-const Usuarios = lazy(() => import("@/featuures/Usuarios/Pages/TableUsers.tsx"));
+const Usuarios = lazy(
+  () => import("@/featuures/Usuarios/Pages/TableUsers.tsx")
+);
 import RecoverLetterPage from "@/featuures/RecoverLetter/Pages/RecoverLetterPage.tsx";
 import RecoverLastPage from "@/featuures/AuditRecoveryLetter/Pages/AuditRecoveryLetterPage.tsx";
-const RegistrarUsuarios = lazy(() => import("@/featuures/RegisterUser/Page/RegisterUser.tsx"));
+import TablaRegistrosAuditados from "@/featuures/Auditados/Pages/TablaRegistrosAuditados";
+const RegistrarUsuarios = lazy(
+  () => import("@/featuures/RegisterUser/Page/RegisterUser.tsx")
+);
 
 //*Lazy Principale Tables
 const TableRoutes = lazy(() => import("@/components/Routes/TablesRoutes.tsx"));
-const TablaCirugias = lazy(() => import("@/featuures/Cirugia/Pages/TablaCirugia.tsx"));
-const TablaAuditoria = lazy(() => import("@/featuures/Auditoria/Pages/TablaAuditoria.tsx"));
-const TablaRadicacion = lazy(() => import("@/featuures/Radicacion/Page/TablaRadicacion.tsx"));
-const TablaAutorizarServicios = lazy(() => import("@/featuures/AuthorizeService/Pages/TablaAutorizarServicios.tsx"));
-const FileManager = lazy(() => import("@/featuures/SystemGC/Page/SistemaArchivosSGC.tsx"));
-const SistemaInventario = lazy(() => import("@/featuures/SystemInventory/Pages/InventorySystem.tsx"));
-const RegistroUsuarios = lazy(() => import("@/featuures/UserRegister/Page/RegistroUsuario.tsx"));
-const GestionTickets = lazy(() => import("@/featuures/HelpDesk/Pages/ProcessHelpDesk.tsx"));
+const TablaCirugias = lazy(
+  () => import("@/featuures/Cirugia/Pages/TablaCirugia.tsx")
+);
+const TablaAuditoria = lazy(
+  () => import("@/featuures/Auditoria/Pages/TablaAuditoria.tsx")
+);
+const TablaRadicacion = lazy(
+  () => import("@/featuures/Radicacion/Page/TablaRadicacion.tsx")
+);
+const TablaAutorizarServicios = lazy(
+  () => import("@/featuures/AuthorizeService/Pages/TablaAutorizarServicios.tsx")
+);
+const FileManager = lazy(
+  () => import("@/featuures/SystemGC/Page/SistemaArchivosSGC.tsx")
+);
+const SistemaInventario = lazy(
+  () => import("@/featuures/SystemInventory/Pages/InventorySystem.tsx")
+);
+const RegistroUsuarios = lazy(
+  () => import("@/featuures/UserRegister/Page/RegistroUsuario.tsx")
+);
+const GestionTickets = lazy(
+  () => import("@/featuures/HelpDesk/Pages/ProcessHelpDesk.tsx")
+);
 
 // Componente para rutas envueltas en el contexto de usuarios
 const UsersPage = () => (
@@ -54,7 +75,14 @@ const ContextualizedRoutes: React.FC = () => {
         <Route path="/tabla-cirugias" element={<TablaCirugias />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/registrar-usuarios" element={<RegistrarUsuarios />} />
-        <Route path="/tabla-autorizar-servicios" element={<TablaAutorizarServicios />} />
+        <Route
+          path="/tabla-autorizar-servicios"
+          element={<TablaAutorizarServicios />}
+        />
+        <Route
+          path="/tabla-registros-auditados"
+          element={<TablaRegistrosAuditados />}
+        />
         <Route path="/SistemGestionCalidad" element={<FileManager />} />
         <Route path="/carta-recobro" element={<RecoverLetterPage />} />
         <Route path="/auditoria" element={<RecoverLastPage />} />
@@ -67,7 +95,7 @@ const ContextualizedRoutes: React.FC = () => {
 
         {/* Rutas de las tablas radicacion - asegurándose que no entre en conflicto */}
         <Route path="/tablas/*" element={<TableRoutes />} />
-        
+
         {/* Fallback route - debe ser la última */}
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
