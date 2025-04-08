b662a939cbd13e3c1549dae923630e10a681344c
DuvalG25 <Duvalgalvis25@gmail.com>
2025-04-08 14:01:06 -0500

    feat: mejorar la visualización de servicios contratados con diseño responsivo y ajustes en la lógica de mapeo

--- src/featuures/home/components/ConsultarPacientesCS.tsx
+++ src/featuures/home/components/ConsultarPacientesCS.tsx
@@ -11,8 +11,7 @@ const FormPacientesCS = () => {
 
   return (
     <>
-      <div className="flex flex-col w-full p-2 pb-3 mb-3 ml-4">
-       
+      <div className="flex flex-col w-full p-2 pb-3 mb-3">
         <div className="flex flex-col">
           <label
             className="w-5/6 dark:text-gray-200 text-[19px] md:text-[24px]"
@@ -30,58 +29,161 @@ const FormPacientesCS = () => {
               placeholder="Numero documento del paciente"
               className="p-2 mt-3 mr-4 border-2 border-gray-400 rounded-lg b-2 w-44 md:w-[272px] dark:text-gray-700 text-[17px]"
             />
-            <button 
-            type="button" 
-            onClick={() => getData(identification)}
-            className="px-5 font-bold text-white bg-teal-500 rounded-md shadow-md mt-23 py-2 hover:bg-teal-600 hover:shadow-teal-600 dark:hover:shadow-teal-500 text-[16px]"
+            <button
+              type="button"
+              onClick={() => getData(identification)}
+              className="px-5 font-bold text-white bg-teal-500 rounded-md shadow-md mt-23 py-2 hover:bg-teal-600 hover:shadow-teal-600 dark:hover:shadow-teal-500 text-[16px]"
             >
               Consultar
             </button>
           </div>
         </div>
-        <div className="w-full mt-5">
+        <div className="hidden w-full mt-5 md:flex justify-items-center dark:text-gray-200">
           {patients ? (
-            <table className="w-full overflow-x-auto border rounded-lg table-auto" key={patients.id}>
+            <table
+              className="w-full overflow-x-auto border rounded-lg table-auto"
+              key={patients.id}
+            >
               <thead>
                 <tr className="text-gray-700 bg-gray-400 dark:bg-gray-800 dark:text-gray-200">
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Tipo Documento</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">N* Documento</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">1* Apellido</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">2* Apellido</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">1* Nombre</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">2* Nombre</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Fecha Nacimiento</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Genero</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">TPS RGM ID</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Direccion</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Telefono</th>
-                  <th  className="px-2 py-3 text-center whitespace-nowrap">Regimen</th>
+                  <th  className="px-2 py-3 text-center">Tipo Documento</th>
+                  <th  className="px-2 py-3 text-center">N* Documento</th>
+                  <th  className="px-2 py-3 text-center">1* Apellido</th>
+                  <th  className="px-2 py-3 text-center">2* Apellido</th>
+                  <th  className="px-2 py-3 text-center">1* Nombre</th>
+                  <th  className="px-2 py-3 text-center">2* Nombre</th>
+                  <th  className="px-2 py-3 text-center">Fecha Nacimiento</th>
+                  <th  className="px-2 py-3 text-center">Genero</th>
+                  <th  className="px-2 py-3 text-center">TPS RGM ID</th>
+                  <th  className="px-2 py-3 text-center">Direccion</th>
+                  <th  className="px-2 py-3 text-center">Telefono</th>
+                  <th  className="px-2 py-3 text-center">Regimen</th>
                 </tr>
               </thead>
               <tbody>
                 <tr  className="bg-gray-200 border-teal-700 dark:text-white dark:bg-gray-700">
-                  <td className="px-6 py-3 font-medium text-center">{patients.tpsIdnId}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.hstIdnNumeroIdentificacion}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.aflPrimerApellido}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.aflSegundoApellido}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.aflPrimerNombre}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.aflSegundoNombre}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.aflFechaNacimiento}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.tpsGnrId}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.tpsRgmId}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.direccion}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.telefono}</td>
-                  <td className="px-6 py-3 font-medium text-center">{patients.regimen}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.tpsIdnId}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.hstIdnNumeroIdentificacion}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.aflPrimerApellido}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.aflSegundoApellido}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.aflPrimerNombre}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.aflSegundoNombre}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.aflFechaNacimiento}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.tpsGnrId}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.tpsRgmId}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.direccion}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.telefono}</td>
+                  <td className="px-2 py-3 text-sm font-medium text-center">{patients.regimen}</td>
                 </tr>
               </tbody>
             </table>
           ) : (
             <div>
-                <p className="text-red-500">{errorPatients}</p>
+              <p className="text-red-500">{errorPatients}</p>
             </div>
           )}
         </div>
       </div>
+        
+          {/* // responsive */}
+          <div className="grid grid-cols-1 gap-4 md:hidden">
+            {patients ? (
+              <div
+                key={patients.tpsIdnId}
+                className="w-[95%] place-self-center p-3 bg-gray-200 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-600"
+              >
+                <div className="grid grid-cols-[40%_60%] gap-3 text-sm">
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Tipo de Documento:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.tpsIdnId}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    N° de Documento:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.hstIdnNumeroIdentificacion}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    1* Apellido:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.aflPrimerApellido}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    2* Apellido:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.aflSegundoApellido}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    1* Nombre:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.aflPrimerNombre}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    2* Nombre:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.aflSegundoNombre}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Fecha Nacimiento:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.aflFechaNacimiento}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Genero:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.tpsGnrId}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    TPS RGM ID:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.tpsRgmId}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Direccion:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.direccion}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Telefono:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.telefono}
+                  </div>
+
+                  <div className="font-semibold text-gray-600 dark:text-gray-400/90">
+                    Regimen:
+                  </div>
+                  <div className="text-gray-800 dark:text-gray-100">
+                    {patients.regimen}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <p className="text-red-500">{errorPatients}</p>
+              </div>
+            )}
+          </div>
     </>
   );
 };
