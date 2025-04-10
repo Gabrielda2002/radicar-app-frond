5a06b051872d9bbe5c68bc4e1e3beed535927afe
Gabriel Duarte <duartearias2002@gmail.com>
2025-04-10 14:45:16 -0500

    feat: simplificar la función FormatDate y mejorar el manejo de fechas

--- src/utils/FormatDate.ts
+++ src/utils/FormatDate.ts
@@ -1,25 +1,23 @@
-import { parseISO } from "date-fns";
-import { formatInTimeZone } from "date-fns-tz";
+import { format, parseISO } from "date-fns";
 
-export const FormatDate = (date: Date | string | null, withTime: boolean = true) => {
+// * funcion para formatear la fecha
+export const FormatDate = (date: Date | null, withTime: boolean = true) => {
     if (!date) return "N/A";
     
-    const timeZone = 'America/Bogota';
     const dateformat = withTime ? "yyyy/MM/dd HH:mm" : "yyyy-MM-dd";
 
-    console.log("date", date)
-
+    console.log('fecha que llega' , date)
+    
     try {
-        // Si es string (formato ISO), usar directamente parseISO
         if (typeof date === "string") {
-            return formatInTimeZone(parseISO(date), timeZone, dateformat);
+            return format(parseISO(date), dateformat);
         }
-        
-        // Si es un objeto Date, convertirlo primero a formato ISO y luego formatearlo
-        // Esto evita problemas con la zona horaria local
-        const isoString = date.toISOString();
-        return formatInTimeZone(parseISO(isoString), timeZone, dateformat);
+        console.log("lo que retorna", format(date, dateformat))
+
+        return format(date, dateformat)
     } catch (error) {
+        // console.log('Error al formatear la fecha:', error, 'valor recibido:', date)
         return "N/A";
     }
+    
 };
