b0fd0d0b5c089759ee966f0d3bc95b5abd69c8bc
Gabriel Duarte <duartearias2002@gmail.com>
2025-05-09 09:48:32 -0500

    feat: agregar métodos para renderizar información básica, técnica y adicional de equipos en el inventario

--- src/featuures/SystemInventory/strategies/EquiposStrategy.tsx
+++ src/featuures/SystemInventory/strategies/EquiposStrategy.tsx
@@ -4,6 +4,8 @@ import { IItems } from '@/models/IItems';
 import ModalItemsForm from '../Components/Modals/ModalItemsForm';
 import ModalTablaseguimientoItem from '../Components/Modals/ModalTablaSeguimientoItem';
 import ModalAccesorioItem from '../Components/Modals/ModalAccesorioItem';
+import { ReactNode } from "react";
+import { FormatDate } from "@/utils/FormatDate";
 
 export class EquiposStrategy implements ItemStrategy<IItems> {
   getName(item: IItems): string {
@@ -59,6 +61,99 @@ export class EquiposStrategy implements ItemStrategy<IItems> {
           {/* Tooltip */}
         </div>
       </>
+    );
+  }
+
+  renderBasicInfo(item: IItems): ReactNode {
+    return (
+      <div className="space-y-2">
+        <li>
+          <strong>Nombre: </strong>
+          {item.nameEquipment}
+        </li>
+        <li>
+          <strong>Tipo: </strong>
+          {item.typeEquipment}
+        </li>
+        <li>
+          <strong>Marca: </strong>
+          {item.brandEquipment}
+        </li>
+        <li>
+          <strong>Modelo: </strong>
+          {item.modelEquipment}
+        </li>
+        <li>
+          <strong>Responsable: </strong>
+          {item.nameUser} {item.lastNameUser}
+        </li>
+        <li>
+          <strong>Candado: </strong>
+          {item.lock ? "Si" : "No"}
+        </li>
+        <li>
+          <strong>Clave: </strong>
+          {item.lockKey}
+        </li>
+      </div>
+    );
+  }
+
+  renderTechnicalDetails(item: IItems): ReactNode {
+    return (
+      <div className="space-y-2">
+        <li>
+          <strong>Serial: </strong>
+          {item.serialEquipment}
+        </li>
+        <li>
+          <strong>DHCP: </strong>
+          {item.dhcp ? "Sí" : "No"}
+        </li>
+        <li>
+          <strong>Direccion IP: </strong>
+          {item.addressIp}
+        </li>
+        <li>
+          <strong>MAC: </strong>
+          {item.mac}
+        </li>
+        <li>
+          <strong>Sistema Operativo: </strong>
+          {item.operationalSystem}
+        </li>
+      </div>
+    );
+  }
+
+  renderAdditionalInfo(item: IItems): ReactNode {
+    return (
+      <div className="space-y-2">
+        <li>
+          <strong>Area: </strong>
+          {item.area}
+        </li>
+        <li>
+          <strong>Fecha de Adquisición: </strong>
+          {FormatDate(item.purchaseDate, false)}
+        </li>
+        <li>
+          <strong>Garantía: </strong>
+          {item.warranty ? "Sí" : "No"}
+        </li>
+        <li>
+          <strong>Timepo de Garantia: </strong>
+          {item.warrantyTime}
+        </li>
+        <li>
+          <strong>Fecha de entrega: </strong>
+          {FormatDate(item.deliveryDate, false)}
+        </li>
+        <li>
+          <strong>Numero de inventario: </strong>
+          {item.inventoryNumberEquipment}
+        </li>
+      </div>
     );
   }
 }
