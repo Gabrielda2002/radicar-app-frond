c84eac5aa3265727736b1d1d68458a54ddd7c547
Gabriel Duarte <duartearias2002@gmail.com>
2025-05-19 16:10:09 -0500

    feat: actualizar icono y título en la sección de inventario de la barra lateral; agregar opción "BIOMETRICO" en títulos de soporte

--- src/components/layout/sidebar/page/SideBar.tsx
+++ src/components/layout/sidebar/page/SideBar.tsx
@@ -27,7 +27,7 @@ import recobro from "/assets/recobro.svg";
 import carta from "/assets/carta.svg";
 import auditoria from "/assets/auditoria.svg";
 import ticket from "/assets/ticket.svg";
-import { Cpu } from "lucide-react";
+import { Box } from "lucide-react";
 
 const SideBar: FC = () => {
   const { isCollapsed, toggleSideBar } = useSidebar();
@@ -189,8 +189,8 @@ const SideBar: FC = () => {
               >
                 <SubCategory
                   to="/SistemaInventario"
-                  icon={Cpu}
-                  title="Inventario Tecnologico"
+                  icon={Box}
+                  title="Inventario General"
                   isCollapsed={isCollapsed}
                 />
               </Category>
