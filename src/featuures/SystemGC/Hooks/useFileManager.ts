13fedc7c555e832112fe9c383e41524ec8fdf625
gabrielda2002 <duartearias2002@gmail.com>
2025-10-01 14:50:00 -0500

    feat: mejorar manejo de errores en useFileManager y se quita municipio de los params que se envian en la solicitudes de la api

--- src/featuures/SystemGC/Hooks/useFileManager.ts
+++ src/featuures/SystemGC/Hooks/useFileManager.ts
@@ -1,172 +1,251 @@
 import { useState, useEffect } from "react";
-import { api, createFolder, deleteItem, downloadFile, getFolderContent, renameItems, uploadFile } from "@/utils/api-config";
+import {
+  api,
+  createFolder,
+  deleteItem,
+  downloadFile,
+  getFolderContent,
+  renameItems,
+  uploadFile,
+} from "@/utils/api-config";
 
-interface FileItem { // Renombrado de File a FileItem
-    id: string;
-    name: string;
-    size: number;
-    mimeType: string;
-    createdAt: string;
-    updatedAt: string;
-    path: string;
+interface FileItem {
+  // Renombrado de File a FileItem
+  id: string;
+  name: string;
+  size: number;
+  mimeType: string;
+  createdAt: string;
+  updatedAt: string;
+  path: string;
 }
 
 interface Folder {
-    id: string;
-    name: string;
-    path: string;
-    parentId: string | null;
-    createdAt: string;
-    updatedAt: string;
+  id: string;
+  name: string;
+  path: string;
+  parentId: string | null;
+  createdAt: string;
+  updatedAt: string;
 }
 
 interface FolderContents {
-    files: FileItem[]; // Actualizado a FileItem
-    folders: Folder[];
+  files: FileItem[]; // Actualizado a FileItem
+  folders: Folder[];
 }
 
-export const useFileManager = (section: string, initialFolderId?: string, refreshKey?: number) => {
-    const [currentFolderId, setCurrentFolderId] = useState<string | null>(initialFolderId || null);
-    const [ path, setPath] = useState<{id: string; name: string}[]>([
-        {id: "", name: "Inicio"}
-    ])
-    const [contents, setContents] = useState<FolderContents | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+export const useFileManager = (
+  section: string,
+  initialFolderId?: string,
+  refreshKey?: number
+) => {
+  const [currentFolderId, setCurrentFolderId] = useState<string | null>(
+    initialFolderId || null
+  );
+  const [path, setPath] = useState<{ id: string; name: string }[]>([
+    { id: "", name: "Inicio" },
+  ]);
+  const [contents, setContents] = useState<FolderContents | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        setPath([{id: "", name: "Inicio"}])
-        setCurrentFolderId(null)
-        setContents(null)
-    } , [section])
+  useEffect(() => {
+    setPath([{ id: "", name: "Inicio" }]);
+    setCurrentFolderId(null);
+    setContents(null);
+  }, [section]);
 
-    // funcion para traer el contenido de la carpeta
-    const fetchContents = async () => {
-        setLoading(true);
-        try {
-            const response = await getFolderContent(section ,currentFolderId || undefined );
-            setContents(response.data);
-            setError(null);
-        } catch (err: any) {
-            setError(`Error al mostrar el contenido de la carpeta. ${err.response?.data?.message}`);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchContents();
-    }, [currentFolderId, section, refreshKey]); // Dependiendo de currentFolderId y section
-
-    useEffect(() => {
-        setCurrentFolderId(initialFolderId || null);
-    }, [initialFolderId]);
-
-    // Function to create a new folder
-    const createNewFolder = async (name: string) => {
-        // if (!currentFolderId) return;
-        try {
-            await createFolder(currentFolderId || null, name, section);
-            await fetchContents(); // Reload contents after creating a folder
-        } catch (err: any) {
-            setError(`Error al crear la carpeta, intente nuevamente. ${err.response?.data?.message}`);
-        }
-    };
-
-    // funcion para subir un archivo
-    const uploadNewFile = async (formData: FormData, id: number | string) => { // Usa el tipo global File ahora
-        // if (!currentFolderId) return;
-        try {
-            await uploadFile(formData, id);
-            await fetchContents(); // Reload contents after uploading a file
-        } catch (err: any) {
-            setError(`Error al subir el archivo, intente nuevamente. ${err.response?.data?.message}`);
-        }
-    };
-
-    // Function to delete an item (folder or file)
-    const deleteItemById = async (id: string, type: "carpetas" | "archivo") => {
-        try {
-            await deleteItem(id, type);
-            await fetchContents(); // Reload contents after deleting an item
-        } catch (err: any) {
-            setError(`Error al eliminar ${type === "carpetas" ? "revisa que la carpeta este vacía" : "file"}, intente nuevamente ${err.response?.data?.message}`);
-        }
-    };
-
-    // Function to download a file
-    const downloadFileById = async (fileId: string, fileName: string) => { 
-        try {
-            const response = await downloadFile(fileId);
-            const url = window.URL.createObjectURL(new Blob([response.data], { type: response.headers['content-type'] }));
-            const link = document.createElement('a');
-            link.href = url;
-    
-            // Set the download attribute with the real file name
-            link.setAttribute('download', fileName || `file_${fileId}`);
-            document.body.appendChild(link);
-            link.click();
-    
-            // Clean up the URL object
-            window.URL.revokeObjectURL(url);
-        } catch (err: any) {
-            setError(`Error downloading file ${err.response?.data?.message || ''}`);
-        }
-    };
-    
-
-    const navigateToFolder = (folderId: string, folderName:string) => {
-        setCurrentFolderId(folderId);
-        setPath((prevPath) => [...prevPath, {id: folderId, name: folderName}])
-    }
-
-    const navigateBackToFolder = (folderId: string) => {
-        setCurrentFolderId(folderId);
-        setPath((prevPath) => {
-            const newPath = prevPath.slice(0, prevPath.findIndex(f => f.id === folderId ) + 1);
-            return newPath;
-        })
-    }
-
-    // * renombrar items del sgc
-
-    const renameItem = async (id: string, name: string, type: "carpetas" | "archivo") => {
-        try {
-            await renameItems(id, currentFolderId || null, name, type);
-            await fetchContents(); // Reload contents after renaming an item
-        } catch (err: any) {
-            setError(`Error al renombrar ${type === "carpetas" ? "la carpeta" : "el archivo"} ${err.response?.data?.message}.`);
-        }
-    }
-
-    const moveItem = async (itemId: number, newParentId: string, type: "carpetas" | "archivos"): Promise<boolean> => {
-        try {
-            setLoading(true);
-            await api.put(`/${type}/${itemId}/move`, { newParentId, section });
-            await fetchContents();
-            return true;
-        } catch (error: any) {
-            setError(`Error al mover ${type === "carpetas" ? "la carpeta" : "el archivo"}: ${error.response?.data?.message}`);
-            console.log(error);
-            return false;
-        }finally{
-            setLoading(false);
-        }
-    }
-
-    return {
-        contents,
-        loading,
-        error,
-        createNewFolder,
-        uploadNewFile,
-        deleteItemById,
-        downloadFileById,
-        setCurrentFolderId: navigateToFolder,
-        navigateBackToFolder,
-        path,
-        renameItem,
+  // funcion para traer el contenido de la carpeta
+  const fetchContents = async () => {
+    setLoading(true);
+    try {
+      const response = await getFolderContent(
         section,
-        moveItem
-    };
+        currentFolderId || undefined
+      );
+      setContents(response.data);
+      setError(null);
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(
+          err.response?.data?.message ||
+            "Error al cargar el contenido de la carpeta."
+        );
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchContents();
+  }, [currentFolderId, section, refreshKey]); // Dependiendo de currentFolderId y section
+
+  useEffect(() => {
+    setCurrentFolderId(initialFolderId || null);
+  }, [initialFolderId]);
+
+  // Function to create a new folder
+  const createNewFolder = async (name: string) => {
+    // if (!currentFolderId) return;
+    try {
+      await createFolder(currentFolderId || null, name, section);
+      await fetchContents(); // Reload contents after creating a folder
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(`Error al crear la carpeta: ${err.response?.data?.message}`);
+      }
+    }
+  };
+
+  // funcion para subir un archivo
+  const uploadNewFile = async (formData: FormData, id: number | string) => {
+    // Usa el tipo global File ahora
+    // if (!currentFolderId) return;
+    try {
+      await uploadFile(formData, id);
+      await fetchContents(); // Reload contents after uploading a file
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(`Error al subir el archivo: ${err.response?.data?.message}`);
+      }
+    }
+  };
+
+  // Function to delete an item (folder or file)
+  const deleteItemById = async (id: string, type: "carpetas" | "archivo") => {
+    try {
+      await deleteItem(id, type);
+      await fetchContents(); // Reload contents after deleting an item
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(
+          `Error al eliminar ${
+            type === "carpetas" ? "la carpeta" : "el archivo"
+          }: ${err.response?.data?.message}`
+        );
+      }
+    }
+  };
+
+  // Function to download a file
+  const downloadFileById = async (fileId: string, fileName: string) => {
+    try {
+      const response = await downloadFile(fileId);
+      const url = window.URL.createObjectURL(
+        new Blob([response.data], { type: response.headers["content-type"] })
+      );
+      const link = document.createElement("a");
+      link.href = url;
+
+      // Set the download attribute with the real file name
+      link.setAttribute("download", fileName || `file_${fileId}`);
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up the URL object
+      window.URL.revokeObjectURL(url);
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(err.response?.data?.message);
+      }
+    }
+  };
+
+  const navigateToFolder = (folderId: string, folderName: string) => {
+    setCurrentFolderId(folderId);
+    setPath((prevPath) => [...prevPath, { id: folderId, name: folderName }]);
+  };
+
+  const navigateBackToFolder = (folderId: string) => {
+    setCurrentFolderId(folderId);
+    setPath((prevPath) => {
+      const newPath = prevPath.slice(
+        0,
+        prevPath.findIndex((f) => f.id === folderId) + 1
+      );
+      return newPath;
+    });
+  };
+
+  // * renombrar items del sgc
+
+  const renameItem = async (
+    id: string,
+    name: string,
+    type: "carpetas" | "archivo"
+  ) => {
+    try {
+      await renameItems(id, currentFolderId || null, name, type);
+      await fetchContents(); // Reload contents after renaming an item
+    } catch (err: any) {
+      if (err.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(err.response?.data?.message);
+      }
+    }
+  };
+
+  const moveItem = async (
+    itemId: number,
+    newParentId: string,
+    type: "carpetas" | "archivos"
+  ): Promise<boolean> => {
+    try {
+      setLoading(true);
+      await api.put(`/${type}/${itemId}/move`, { newParentId, section });
+      await fetchContents();
+      return true;
+    } catch (error: any) {
+      if (error.response?.status === 500) {
+        setError(
+          "Error del servidor. Por favor, intente nuevamente más tarde."
+        );
+      } else {
+        setError(error.response?.data?.message);
+      }
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    contents,
+    loading,
+    error,
+    createNewFolder,
+    uploadNewFile,
+    deleteItemById,
+    downloadFileById,
+    setCurrentFolderId: navigateToFolder,
+    navigateBackToFolder,
+    path,
+    renameItem,
+    section,
+    moveItem,
+  };
 };
--- src/utils/api-config.ts
+++ src/utils/api-config.ts
@@ -1,285 +1,306 @@
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem("token");
 
 export const api = axios.create({
-    baseURL: `${import.meta.env.VITE_URL_BACKEND}/api/v1`,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token ? `Bearer ${token}` : undefined
-    }
-})
+  baseURL: `${import.meta.env.VITE_URL_BACKEND}/api/v1`,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: token ? `Bearer ${token}` : undefined,
+  },
+});
 
 let isRefreshing = false;
 let failedQueue: any[] = [];
 
 const processQueue = (error: any, token: string | null = null) => {
-    failedQueue.forEach((prom) => {
-        if (error) {
-            prom.reject(error);
-        }else {
-            prom.resolve(token);
-        }
-    });
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
 
-    failedQueue = [];
-}
+  failedQueue = [];
+};
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token')
-        const rol = localStorage.getItem('rol')
+  (config) => {
+    const token = localStorage.getItem("token");
+    const rol = localStorage.getItem("rol");
 
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`
-        }else if(rol){
-            config.headers['Authorization'] = `Bearer ${rol}`
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    } else if (rol) {
+      config.headers["Authorization"] = `Bearer ${rol}`;
     }
-)
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
 
-        const originalRequest = error.config;
-        
-        if (error.response.status === 401 && error.response?.data?.code === "TOKEN_EXPIRED" && !originalRequest._retry) {
-            if (isRefreshing) {
-                return new Promise((resolve, reject) => {
-                    failedQueue.push({ resolve, reject});
-                }).then(token => {
-                    originalRequest.headers['Authorization'] = `Bearer ${token}`;
-                    return api(originalRequest);
-                }).catch((err) => {
-                    return Promise.reject(err);
-                });
-            }
+    if (
+      error.response.status === 401 &&
+      error.response?.data?.code === "TOKEN_EXPIRED" &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            return api(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
 
-            originalRequest._retry = true;
-            isRefreshing = true;
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-            try {
-                
-                const refreshResponse = await api.post('/refresh');
-                const { accessToken } = refreshResponse.data;
+      try {
+        const refreshResponse = await api.post("/refresh");
+        const { accessToken } = refreshResponse.data;
 
-                localStorage.setItem('token', accessToken)
-                
-                api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        localStorage.setItem("token", accessToken);
 
-                processQueue(null, accessToken);
+        api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-                originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+        processQueue(null, accessToken);
 
-                return api(originalRequest);
+        originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
 
-            } catch (error) {
-                processQueue(error, null);
+        return api(originalRequest);
+      } catch (error) {
+        processQueue(error, null);
 
-                localStorage.removeItem('token');
-                localStorage.removeItem('rol');
-                localStorage.removeItem('Municipio');
-                localStorage.removeItem('user');
+        localStorage.removeItem("token");
+        localStorage.removeItem("rol");
+        localStorage.removeItem("Municipio");
+        localStorage.removeItem("user");
 
-                const currentPath = window.location.pathname;
-                sessionStorage.setItem('redirectPath', currentPath);
+        const currentPath = window.location.pathname;
+        sessionStorage.setItem("redirectPath", currentPath);
 
-                toast.error('Tu sesión ha expirado. Por favor, inicia sesión nuevamente.', {
-                    position: 'top-center',
-                    autoClose: 3000,
-                });
+        toast.error(
+          "Tu sesión ha expirado. Por favor, inicia sesión nuevamente.",
+          {
+            position: "top-center",
+            autoClose: 3000,
+          }
+        );
 
-                window.location.href = '/login';
-
-                return Promise.reject(error);
-            }finally{
-                isRefreshing = false;
-            }
-        }
-
-        if (error.response.status === 401 && error.response?.data?.code !== "TOKEN_EXPIRED") {
-            toast.error('No tienes permisos para realizar esta acción.', {
-                position: 'top-center',
-                autoClose: 3000,
-            });
-        }
+        window.location.href = "/login";
 
         return Promise.reject(error);
-
+      } finally {
+        isRefreshing = false;
+      }
     }
-)
+
+    if (
+      error.response.status === 401 &&
+      error.response?.data?.code !== "TOKEN_EXPIRED"
+    ) {
+      toast.error("No tienes permisos para realizar esta acción.", {
+        position: "top-center",
+        autoClose: 3000,
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export const getFolderContent = async (section: string, folderId?: string) => {
-    const idMunicipio = localStorage.getItem('Municipio')
-    const path = folderId ? `/sistema-calidad/${folderId}` : '/sistema-calidad';
-    return api.get(path, {
-        params: {
-            Municipio: idMunicipio,
-            section: section
-        },
-    });
+  const path = folderId ? `/sistema-calidad/${folderId}` : "/sistema-calidad";
+  return api.get(path, {
+    params: {
+      section: section,
+    },
+  });
 };
 
+export const createFolder = async (
+  parentFolderId: string | null,
+  name: string,
+  section: string = "sgc"
+) => {
+  console.log(name);
 
-export const createFolder = async (parentFolderId: string | null, name: string, section: string  = "sgc") => {
-    console.log(name)
-    const idMunicipio = localStorage.getItem('Municipio')
+  const datosUsuario = JSON.parse(localStorage.getItem("user") || "{}");
+  const idUsuario = datosUsuario.id;
 
-    const datosUsuario = JSON.parse(localStorage.getItem('user') || '{}');
-    const idUsuario = datosUsuario.id;
+  const folderName = name;
 
-    const folderName = name;
-
-    return api.post('/carpetas', {parentFolderId, folderName, municipio: idMunicipio, user_id: idUsuario, section });
-}
-
-export const uploadFile = async (formData: FormData, id: number | string, section: string = 'sgc') => {
-
-    formData.append('section', section);
-
-    return api.post(`/archivo`, formData, {
-        params: {
-            parentFolderId: id, // Pasar el ID de la carpeta en los parámetros de la URL
-            section: section
-        },
-        headers: {
-            'Content-Type': 'multipart/form-data',
-        },
-    });
+  return api.post("/carpetas", {
+    parentFolderId,
+    folderName,
+    user_id: idUsuario,
+    section,
+  });
 };
 
+export const uploadFile = async (
+  formData: FormData,
+  id: number | string,
+  section: string = "sgc"
+) => {
+  formData.append("section", section);
+
+  return api.post(`/archivo`, formData, {
+    params: {
+      parentFolderId: id, // Pasar el ID de la carpeta en los parámetros de la URL
+      section: section,
+    },
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
 export const deleteItem = (id: string, type: "carpetas" | "archivo") => {
-    return api.delete(`/${type}/${id}`);
+  return api.delete(`/${type}/${id}`);
 };
 
-
 export const downloadFile = (id: string) => {
-    console.log(id)
-    return api.get(`/download-file/${id}`, {
-        responseType: 'blob'
-    })
-}
+  console.log(id);
+  return api.get(`/download-file/${id}`, {
+    responseType: "blob",
+  });
+};
 
 // * funciones para cambiar el nombre de carpetas y archivos
 
-export const renameItems = async (folderId: string, parentFolderId: string  | null , newName: string, type: "carpetas" | "archivo" ) => {
-    console.log(folderId, parentFolderId, newName, type)
-    return api.put(`/${type}/${folderId}`, { name: newName, parentFolderId });
+export const renameItems = async (
+  folderId: string,
+  parentFolderId: string | null,
+  newName: string,
+  type: "carpetas" | "archivo"
+) => {
+  console.log(folderId, parentFolderId, newName, type);
+  return api.put(`/${type}/${folderId}`, { name: newName, parentFolderId });
 };
 
 // actualizar datos del paciente
 
 export const updatePatientData = async (data: FormData, id: string) => {
-    return api.put(`/pacientes/${id}`, data);
+  return api.put(`/pacientes/${id}`, data);
 };
 
 // guardar datos a radicar
 export const saveRadicar = async (data: FormData) => {
-    return api.post(`/radicacion`, data);
-}
+  return api.post(`/radicacion`, data);
+};
 
 // guardar el archivo soporte de radicar
 export const saveFileRadicar = async (file: FormData) => {
-    return api.post(`/soportes`, file, {
-        headers: {
-            'Content-Type': 'multipart/form-data',
-        },
-    });
-}
+  return api.post(`/soportes`, file, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
 // guardar CUPS
 export const saveCups = async (data: FormData) => {
-    return api.post(`/cups-radicados`, data);
-}
+  return api.post(`/cups-radicados`, data);
+};
 
 export const AuditarRadicado = async (data: object, id: number) => {
-    return api.put(`/autorizar-radicado/${id}`, data)
-}
+  return api.put(`/autorizar-radicado/${id}`, data);
+};
 
 export const autorizarCups = async (data: object, id: number) => {
-    return api.put(`/autorizar-cups/${id}`, data, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-}
+  return api.put(`/autorizar-cups/${id}`, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export const createUsuario = async (data: FormData) => {
-    return api.post(`/usuarios`, data);
-}
+  return api.post(`/usuarios`, data);
+};
 
 // actualizar datos del perfil del usuario
 export const updateUserDataEp = async (data: FormData, id: number) => {
-    return api.put(`/usuario-datos-basicos/${id}`, data);
-}
+  return api.put(`/usuario-datos-basicos/${id}`, data);
+};
 
 // crear CUPS
 export const createCupsEp = async (data: FormData, ep: string) => {
-    return api.post(`/${ep}`, data);
-}
+  return api.post(`/${ep}`, data);
+};
 
 export const createProgramacionCirugia = async (data: FormData) => {
-    return api.post(`/cirugias`, data);
-}
+  return api.post(`/cirugias`, data);
+};
 
 // actualizar la contrasena del usuario=
 export const updatePasswordUsuarioEp = async (data: FormData, id: number) => {
-    return api.put(`/usuario-update-password/${id}`, data);
-}
+  return api.put(`/usuario-update-password/${id}`, data);
+};
 
 // crear item sistema inventario
 export const createItemEp = async (data: FormData, ep: string) => {
-
-    const configHeader = ep !== 'dispositivos-red'
-    ? {
-        headers: {
-            'Content-Type': 'multipart/form-data',
+  const configHeader =
+    ep !== "dispositivos-red"
+      ? {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-    } : {};
+      : {};
 
-    return api.post(`/${ep}`, data, configHeader);
-}
+  return api.post(`/${ep}`, data, configHeader);
+};
 
 // actualizar item sistema inventario
 export const updateItemEp = async (data: FormData, id: number, ep: string) => {
-
-    const configHeader = ep !== 'dispositivos-red'
-    ? {
-        headers: {
-            'Content-Type': 'multipart/form-data',
+  const configHeader =
+    ep !== "dispositivos-red"
+      ? {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-    } : {};
+      : {};
 
-    return api.put(`/${ep}/${id}`, data, configHeader);
-}
+  return api.put(`/${ep}/${id}`, data, configHeader);
+};
 
 // crear seguimiento item
 export const createSeguimientoItemEp = async (data: FormData, ep: string) => {
-    return api.post(`/${ep}`, data);
-}
+  return api.post(`/${ep}`, data);
+};
 
 // crear accesorio item
 export const createAccesoryEquipmentEp = async (data: FormData, ep: string) => {
-    return api.post(`/${ep}`, data);
-}
+  return api.post(`/${ep}`, data);
+};
 
 // crear observacion item
 export const createActiveBreakesEp = async (data: FormData) => {
-    return api.post(`/active-brakes`, data);
-}
+  return api.post(`/active-brakes`, data);
+};
 
 // crear evento
 export const createEventEp = async (data: FormData) => {
-    return api.post(`/eventos`, data);
-}
+  return api.post(`/eventos`, data);
+};
