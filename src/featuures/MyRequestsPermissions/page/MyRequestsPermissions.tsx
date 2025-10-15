723727e1ed17fff3bc784837bff5160a9b64364c
Gabriel Duarte <duartearias2002@gmail.com>
2025-10-15 17:05:46 -0500

    feat: mejorar la presentación de solicitudes en MyRequestsPermissions con detalles adicionales y formato consistente

--- src/featuures/MyRequestsPermissions/page/MyRequestsPermissions.tsx
+++ src/featuures/MyRequestsPermissions/page/MyRequestsPermissions.tsx
@@ -51,7 +51,10 @@ const MyRequestsPermissions: React.FC<MyRequestsPermissionsProps> = ({
       >
         <div className="grid grid-cols-2 gap-5 p-4">
           {data.map((r) => (
-            <div className="grid md:grid-cols-2 gap-2 p-4 border border-gray-300 dark:border-gray-900 rounded-md shadow-md">
+            <div
+              key={r.id}
+              className="grid md:grid-cols-2 gap-2 p-4 border border-gray-300 dark:border-gray-900 rounded-md shadow-md"
+            >
               <div className="flex items-center justify-between col-span-2">
                 <h5>{r.category}</h5>
                 <div
@@ -64,38 +67,91 @@ const MyRequestsPermissions: React.FC<MyRequestsPermissionsProps> = ({
                 </div>
               </div>
               <div className="col-span-2 text-center">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">Granularidad:</h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">{r.granularity}</p>
+                <h5 className="text-base text-gray-800 dark:text-gray-100">
+                  Granularidad:
+                </h5>
+                <p className="text-sm text-gray-400 dark:text-gray-500">
+                  {r.granularity}
+                </p>
               </div>
               <div>
-                <h5 className="text-base text-gray-800 dark:text-gray-100">Fecha de inicio:</h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">{FormatDate(r.startDate, false)}</p>
+                <h5 className="text-base text-gray-800 dark:text-gray-100">
+                  Fecha de inicio:
+                </h5>
+                <p className="text-sm text-gray-400 dark:text-gray-500">
+                  {FormatDate(r.startDate, false)}
+                </p>
               </div>
               <div>
-                <h5 className="text-base text-gray-800 dark:text-gray-100">Fecha de fin:</h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">{FormatDate(r.endDate, false)}</p>
+                <h5 className="text-base text-gray-800 dark:text-gray-100">
+                  Fecha de fin:
+                </h5>
+                <p className="text-sm text-gray-400 dark:text-gray-500">
+                  {FormatDate(r.endDate, false)}
+                </p>
               </div>
               <div className=" col-span-2 text-center">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">Dias Solicitados:</h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">{r.requestedDays}</p>
+                <h5 className="text-base text-gray-800 dark:text-gray-100">
+                  Dias Solicitados:
+                </h5>
+                <p className="text-sm text-gray-400 dark:text-gray-500">
+                  {r.requestedDays}
+                </p>
               </div>
               {r.startTime != "00:00:00" ||
                 (r.startTime == null && (
                   <div>
-                    <h5 className="text-base text-gray-800 dark:text-gray-100">Hora de inicio:</h5>
-                    <p className="text-sm text-gray-400 dark:text-gray-500">{r.startTime}</p>
+                    <h5 className="text-base text-gray-800 dark:text-gray-100">
+                      Hora de inicio:
+                    </h5>
+                    <p className="text-sm text-gray-400 dark:text-gray-500">
+                      {r.startTime}
+                    </p>
                   </div>
                 ))}
               {r.endTime != "00:00:00" ||
                 (r.endTime == null && (
                   <div>
-                    <h5 className="text-base text-gray-800 dark:text-gray-100">Hora de fin:</h5>
-                    <p className="text-sm text-gray-400 dark:text-gray-500">{r.endTime}</p>
+                    <h5 className="text-base text-gray-800 dark:text-gray-100">
+                      Hora de fin:
+                    </h5>
+                    <p className="text-sm text-gray-400 dark:text-gray-500">
+                      {r.endTime}
+                    </p>
                   </div>
                 ))}
               <div className="col-span-2 text-center dark:bg-gray-700 rounded-lg">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">Nota:</h5>
-                <p className="text-sm text-gray-400 dark:text-gray-300">{r.notes}</p>
+                <h5 className="text-base text-gray-800 dark:text-gray-100">
+                  Nota:
+                </h5>
+                <p className="text-sm text-gray-400 dark:text-gray-300">
+                  {r.notes}
+                </p>
+              </div>
+              <div className="col-span-2">
+                <div>
+                  {r.stepsRelation.map((s) => (
+                    <div key={s.id}>
+                      <h5 className="text-lg text-gray-800 dark:text-gray-100">Paso #{s.order}:</h5>
+                    <div
+                      className="grid grid-cols-2 items-center justify-between p-2 my-1 border border-gray-300 rounded-md dark:border-gray-700"
+                    >
+                      <div>
+                        <h5 className="text-base text-gray-800 dark:text-gray-100">Estado:</h5>
+                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.status}</p>
+                      </div>
+                      <div>
+                        <h5 className="text-base text-gray-800 dark:text-gray-100">Tipo de paso:</h5>
+                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.stepType}</p>
+                      </div>
+                      <div className="col-span-2 text-center">
+                        <h5 className="text-base text-gray-800 dark:text-gray-100">Comentario:</h5>
+                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.comment}</p>
+                      </div>
+                    </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
