fb36bf7cdc87e26691d2af6d0fd9914b4cb58512
Gabriel Duarte <duartearias2002@gmail.com>
2025-10-20 10:52:01 -0500

    feat: paginacion, busqueda y validacion de datos en componente modulo de permisos

--- src/featuures/MyRequestsPermissions/page/MyRequestsPermissions.tsx
+++ src/featuures/MyRequestsPermissions/page/MyRequestsPermissions.tsx
@@ -1,15 +1,40 @@
 import ModalDefault from "@/components/common/Ui/ModalDefault";
-import React from "react";
-import { MyRequestsPermissionsProps } from "../types/MyRquestsPermissions,.type";
+import React, { useState } from "react";
+import {
+  IMyRequestsPermissions,
+  MyRequestsPermissionsProps,
+} from "../types/MyRquestsPermissions,.type";
 import { useFetchMyRequests } from "../hook/useFetctMyRequests";
 import { FormatDate } from "@/utils/FormatDate";
 import { BadgeCheck, BadgeAlert, Badge } from "lucide-react";
+import useSearch from "@/hooks/useSearch";
+import usePagination from "@/hooks/usePagination";
+import Input from "@/components/common/Ui/Input";
+import Select from "@/components/common/Ui/Select";
+import Pagination from "@/components/common/PaginationTable/PaginationTable";
+
+const ITEMS_PER_PAGE = 10;
 
 const MyRequestsPermissions: React.FC<MyRequestsPermissionsProps> = ({
   isOpen,
   onClose,
 }) => {
-  const { data, isLoading, error } = useFetchMyRequests();
+  const [itemsPerPage] = useState(ITEMS_PER_PAGE);
+  const { data } = useFetchMyRequests();
+
+  const { query, setQuery, filteredData } = useSearch<IMyRequestsPermissions>(
+    data || [],
+    ["category", "overallStatus", "granularity"]
+  );
+
+  const { currentPage, totalPages, paginate, currentData, setItemsPerPage } =
+    usePagination<IMyRequestsPermissions>(filteredData, itemsPerPage);
+
+  const handleItemsPerPageChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setItemsPerPage(Number(e.target.value));
+  };
 
   // ? funcion para manejar los estilos de estado
   const handleStatus = (status: string) => {
@@ -47,115 +72,158 @@ const MyRequestsPermissions: React.FC<MyRequestsPermissionsProps> = ({
         isOpen={isOpen}
         onClose={onClose}
         title="Mis Solicitudes"
-        size="full"
+        size="lg"
       >
-        <div className="grid grid-cols-2 gap-5 p-4">
-          {data.map((r) => (
-            <div
-              key={r.id}
-              className="grid md:grid-cols-2 gap-2 p-4 border border-gray-300 dark:border-gray-900 rounded-md shadow-md"
-            >
-              <div className="flex items-center justify-between col-span-2">
-                <h5>{r.category}</h5>
-                <div
-                  className={`flex items-center gap-2 px-3 py-1 rounded-full ${
-                    handleStatus(r.overallStatus).style
-                  }`}
-                >
-                  {handleStatus(r.overallStatus).icon}
-                  <span className="font-semibold">{r.overallStatus}</span>
+        {data?.length === 0 ? (
+          <div className="flex justify-center p-4">
+            <p className="text-center text-base text-gray-500">
+              No hay solicitudes de permisos disponibles.
+            </p>
+          </div>
+        ) : (
+        <div className="p-4 space-y-4">
+          <div className="flex items-center gap-3 my-3">
+            <Input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Buscar"
+            />
+            <Select
+              options={[
+                { label: "10", value: "10" },
+                { label: "20", value: "20" },
+                { label: "30", value: "30" },
+              ]}
+              value={itemsPerPage}
+              onChange={handleItemsPerPageChange}
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            {currentData().map((r) => (
+              <div
+                key={r.id}
+                className="grid md:grid-cols-2 gap-2 p-4 border border-gray-300 dark:border-gray-900 rounded-md shadow-md"
+              >
+                <div className="flex items-center justify-between col-span-2">
+                  <h5>{r.category}</h5>
+                  <div
+                    className={`flex items-center gap-2 px-3 py-1 rounded-full ${
+                      handleStatus(r.overallStatus).style
+                    }`}
+                  >
+                    {handleStatus(r.overallStatus).icon}
+                    <span className="font-semibold">{r.overallStatus}</span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-span-2 text-center">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">
-                  Granularidad:
-                </h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">
-                  {r.granularity}
-                </p>
-              </div>
-              <div>
-                <h5 className="text-base text-gray-800 dark:text-gray-100">
-                  Fecha de inicio:
-                </h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">
-                  {FormatDate(r.startDate, false)}
-                </p>
-              </div>
-              <div>
-                <h5 className="text-base text-gray-800 dark:text-gray-100">
-                  Fecha de fin:
-                </h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">
-                  {FormatDate(r.endDate, false)}
-                </p>
-              </div>
-              <div className=" col-span-2 text-center">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">
-                  Dias Solicitados:
-                </h5>
-                <p className="text-sm text-gray-400 dark:text-gray-500">
-                  {r.requestedDays}
-                </p>
-              </div>
-              {r.startTime != "00:00:00" ||
-                (r.startTime == null && (
-                  <div>
-                    <h5 className="text-base text-gray-800 dark:text-gray-100">
-                      Hora de inicio:
-                    </h5>
-                    <p className="text-sm text-gray-400 dark:text-gray-500">
-                      {r.startTime}
-                    </p>
-                  </div>
-                ))}
-              {r.endTime != "00:00:00" ||
-                (r.endTime == null && (
-                  <div>
-                    <h5 className="text-base text-gray-800 dark:text-gray-100">
-                      Hora de fin:
-                    </h5>
-                    <p className="text-sm text-gray-400 dark:text-gray-500">
-                      {r.endTime}
-                    </p>
-                  </div>
-                ))}
-              <div className="col-span-2 text-center dark:bg-gray-700 rounded-lg">
-                <h5 className="text-base text-gray-800 dark:text-gray-100">
-                  Nota:
-                </h5>
-                <p className="text-sm text-gray-400 dark:text-gray-300">
-                  {r.notes}
-                </p>
-              </div>
-              <div className="col-span-2">
+                <div className="col-span-2 text-center">
+                  <h5 className="text-base text-gray-800 dark:text-gray-100">
+                    Granularidad:
+                  </h5>
+                  <p className="text-sm text-gray-400 dark:text-gray-500">
+                    {r.granularity}
+                  </p>
+                </div>
                 <div>
-                  {r.stepsRelation.map((s) => (
-                    <div key={s.id}>
-                      <h5 className="text-lg text-gray-800 dark:text-gray-100">Paso #{s.order}:</h5>
-                    <div
-                      className="grid grid-cols-2 items-center justify-between p-2 my-1 border border-gray-300 rounded-md dark:border-gray-700"
-                    >
-                      <div>
-                        <h5 className="text-base text-gray-800 dark:text-gray-100">Estado:</h5>
-                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.status}</p>
-                      </div>
-                      <div>
-                        <h5 className="text-base text-gray-800 dark:text-gray-100">Tipo de paso:</h5>
-                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.stepType}</p>
-                      </div>
-                      <div className="col-span-2 text-center">
-                        <h5 className="text-base text-gray-800 dark:text-gray-100">Comentario:</h5>
-                        <p className="text-sm text-gray-400 dark:text-gray-500">{s.comment}</p>
-                      </div>
-                    </div>
+                  <h5 className="text-base text-gray-800 dark:text-gray-100">
+                    Fecha de inicio:
+                  </h5>
+                  <p className="text-sm text-gray-400 dark:text-gray-500">
+                    {FormatDate(r.startDate, false)}
+                  </p>
+                </div>
+                <div>
+                  <h5 className="text-base text-gray-800 dark:text-gray-100">
+                    Fecha de fin:
+                  </h5>
+                  <p className="text-sm text-gray-400 dark:text-gray-500">
+                    {FormatDate(r.endDate, false)}
+                  </p>
+                </div>
+                <div className=" col-span-2 text-center">
+                  <h5 className="text-base text-gray-800 dark:text-gray-100">
+                    Dias Solicitados:
+                  </h5>
+                  <p className="text-sm text-gray-400 dark:text-gray-500">
+                    {r.requestedDays}
+                  </p>
+                </div>
+                {r.startTime != "00:00:00" ||
+                  (r.startTime == null && (
+                    <div>
+                      <h5 className="text-base text-gray-800 dark:text-gray-100">
+                        Hora de inicio:
+                      </h5>
+                      <p className="text-sm text-gray-400 dark:text-gray-500">
+                        {r.startTime}
+                      </p>
                     </div>
                   ))}
+                {r.endTime != "00:00:00" ||
+                  (r.endTime == null && (
+                    <div>
+                      <h5 className="text-base text-gray-800 dark:text-gray-100">
+                        Hora de fin:
+                      </h5>
+                      <p className="text-sm text-gray-400 dark:text-gray-500">
+                        {r.endTime}
+                      </p>
+                    </div>
+                  ))}
+                <div className="col-span-2 text-center dark:bg-gray-700 rounded-lg">
+                  <h5 className="text-base text-gray-800 dark:text-gray-100">
+                    Nota:
+                  </h5>
+                  <p className="text-sm text-gray-400 dark:text-gray-300">
+                    {r.notes}
+                  </p>
+                </div>
+                <div className="col-span-2">
+                  <div>
+                    {r.stepsRelation.map((s) => (
+                      <div key={s.id}>
+                        <h5 className="text-lg text-gray-800 dark:text-gray-100">
+                          Paso #{s.order}:
+                        </h5>
+                        <div className="grid grid-cols-2 items-center justify-between p-2 my-1 border border-gray-300 rounded-md dark:border-gray-700">
+                          <div>
+                            <h5 className="text-base text-gray-800 dark:text-gray-100">
+                              Estado:
+                            </h5>
+                            <p className="text-sm text-gray-400 dark:text-gray-500">
+                              {s.status}
+                            </p>
+                          </div>
+                          <div>
+                            <h5 className="text-base text-gray-800 dark:text-gray-100">
+                              Tipo de paso:
+                            </h5>
+                            <p className="text-sm text-gray-400 dark:text-gray-500">
+                              {s.stepType}
+                            </p>
+                          </div>
+                          <div className="col-span-2 text-center">
+                            <h5 className="text-base text-gray-800 dark:text-gray-100">
+                              Comentario:
+                            </h5>
+                            <p className="text-sm text-gray-400 dark:text-gray-500">
+                              {s.comment}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={paginate}
+            />
         </div>
+        )}
       </ModalDefault>
     </>
   );
--- src/featuures/Permission/page/Permission.tsx
+++ src/featuures/Permission/page/Permission.tsx
@@ -3,16 +3,41 @@ import { FormatDate } from "@/utils/FormatDate";
 import LoadingSpinner from "@/components/common/LoadingSpinner/LoadingSpinner";
 import ModalPermissionsActions from "../components/ModalPermissionsActions";
 import { useFetchRequestPermissions } from "../hook/useFetchRequestPermissions";
+import useSearch from "@/hooks/useSearch";
+import { IRequestsPermissions } from "../type/IRequestsPermissions";
+import usePagination from "@/hooks/usePagination";
+import { useState } from "react";
+import Input from "@/components/common/Ui/Input";
+import Select from "@/components/common/Ui/Select";
+import Pagination from "@/components/common/PaginationTable/PaginationTable";
+const ITENS_PER_PAGE = 10;
 
 const Permission = () => {
   const { data, error, isLoading, refetch } = useFetchRequestPermissions();
 
+  const [itemsPerPage] = useState(ITENS_PER_PAGE);
+
+  const { filteredData, setQuery, query} = useSearch<IRequestsPermissions>(data || [], [
+    "category",
+    "requesterName",
+    "overallStatus",
+    "granularity",
+  ]);
+
+  const {currentPage, totalPages, paginate, currentData, setItemsPerPage } = usePagination<IRequestsPermissions>(
+    filteredData, itemsPerPage 
+  );
+
+  const handleItemsPerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setItemsPerPage(Number(e.target.value));
+  }
+
   return (
     <>
       {isLoading ? (
-      <LoadingSpinner  />
+        <LoadingSpinner />
       ) : error != null ? (
-        <div className="text-red-500">Error: {error}</div>
+        <div className="text-red-500 text-base">Error: {error}</div>
       ) : (
         <>
           <ModalSection
@@ -22,55 +47,84 @@ const Permission = () => {
               { label: "Solicitudes de Permisos", path: "/permissions" },
             ]}
           />
-          <div className="w-full max-w-full p-5 ml-0 bg-white rounded-md shadow-lg dark:bg-gray-800 mb-11 shadow-indigo-500/10">
-            <table className="w-full">
-              <thead className="text-gray-400 text-base">
-                <tr>
-                  <th className="text-start">Colaborador</th>
-                  <th className="text-start">Area</th>
-                  <th className="text-start">Categoria</th>
-                  <th className="text-start">Granularidad</th>
-                  <th className="text-start">Fecha Solicitud</th>
-                  <th className="text-start">Estado</th>
-                  <th className="text-center">Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data &&
-                  data.map((p) => (
-                    <tr
-                      className="border-b border-gray-200 dark:border-gray-700"
-                      key={p.id}
-                    >
-                      <td className="text-start py-4 text-gray-950 p-0 bg-gray-50 dark:text-gray-50 dark:bg-gray-800">
-                        {p.requesterName}
-                      </td>
-                      <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
-                        {p.category}
-                      </td>
-                      <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
-                        {p.category}
-                      </td>
-                      <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
-                        {p.granularity}
-                      </td>
-                      <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
-                        {FormatDate(p.createdAt)}
-                      </td>
-                      <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
-                        {p.overallStatus}
-                      </td>
-                      <td className="text-center p-0 bg-gray-50  dark:bg-gray-800">
-                        <ModalPermissionsActions
-                          permission={p}
-                          onSuccess={refetch}
-                        />
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
+          {data?.length === 0 ? (
+            <div>
+              <p className="text-center text-gray-500 mt-10">
+                No hay solicitudes de permisos disponibles.
+              </p>
+            </div>
+          ) : (
+            <div className="w-full max-w-full p-5 ml-0 bg-white rounded-md shadow-lg dark:bg-gray-800 mb-11 shadow-indigo-500/10">
+              <div className="flex items-center my-5 gap-3">
+                <Input
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  placeholder="Buscar"
+                />
+                <Select
+                  options={[
+                    { label: "10", value: "10" },
+                    { label: "20", value: "20" },
+                    { label: "30", value: "30" },
+                  ]}
+                  value={itemsPerPage}
+                  onChange={handleItemsPerPageChange}
+                />
+              </div>
+              <table className="w-full">
+                <thead className="text-gray-400 text-base">
+                  <tr>
+                    <th className="text-start">Colaborador</th>
+                    <th className="text-start">Area</th>
+                    <th className="text-start">Categoria</th>
+                    <th className="text-start">Granularidad</th>
+                    <th className="text-start">Fecha Solicitud</th>
+                    <th className="text-start">Estado</th>
+                    <th className="text-center">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data &&
+                    currentData().map((p) => (
+                      <tr
+                        className="border-b border-gray-200 dark:border-gray-700"
+                        key={p.id}
+                      >
+                        <td className="text-start py-4 text-gray-950 p-0 bg-gray-50 dark:text-gray-50 dark:bg-gray-800">
+                          {p.requesterName}
+                        </td>
+                        <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
+                          {p.category}
+                        </td>
+                        <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
+                          {p.category}
+                        </td>
+                        <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
+                          {p.granularity}
+                        </td>
+                        <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
+                          {FormatDate(p.createdAt)}
+                        </td>
+                        <td className="text-start text-gray-500 p-0 bg-gray-50  dark:bg-gray-800">
+                          {p.overallStatus}
+                        </td>
+                        <td className="text-center p-0 bg-gray-50  dark:bg-gray-800">
+                          <ModalPermissionsActions
+                            permission={p}
+                            onSuccess={refetch}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+              <Pagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                onPageChange={paginate}
+              />
+            </div>
+          )}
         </>
       )}
     </>
