0f277b1f19ca9545711f54eebc68e5563ccb0fcf
Gabriel Duarte <duartearias2002@gmail.com>
2025-11-12 15:13:35 -0500

    feat: mejorar el componente ModalConfigBalance para mostrar fechas de inicio y fin de los balances

--- src/featuures/ConfigurationVacations/Components/ModalConfigBalance.tsx
+++ src/featuures/ConfigurationVacations/Components/ModalConfigBalance.tsx
@@ -8,6 +8,7 @@ import Button from "@/components/common/Ui/Button";
 import { useBalancesMutations } from "../Hooks/useBalancesMutations";
 import { toast } from "react-toastify";
 import { AnimatePresence } from "framer-motion";
+import { FormatDate } from "@/utils/FormatDate";
 
 const ModalConfigBalance: React.FC<ModalConfigBalanceProps> = ({
   balances,
@@ -81,44 +82,78 @@ const ModalConfigBalance: React.FC<ModalConfigBalanceProps> = ({
         isSubmitting={formik.isSubmitting || isLoading}
         isValid={formik.isValid}
       >
-        <div className="space-y-4 grid grid-cols-1">
-          {formik.values.configuration.map((c, index) => (
-            <div
-              key={index}
-              className="grid md:grid-cols-2 grid-cols-1 gap-4 py-3 px-4"
-            >
-              <Input
-                type="number"
-                label={`Días Tomados para Balance ID: ${c.balanceId}`}
-                name={`configuration[${index}].diasTomados`}
-                value={formik.values.configuration[index].diasTomados}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={getFieldError(index, "diasTomados")}
-                touched={formik.touched.configuration?.[index]?.diasTomados}
-                required
-              />
-              <Input
-                label="Notas (Opcional)"
-                name={`configuration[${index}].notas`}
-                value={formik.values.configuration[index].notas}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={getFieldError(index, "notas")}
-                touched={formik.touched.configuration?.[index]?.notas}
-              />
-            </div>
-          ))}
-          <AnimatePresence>
-            {error && (
-              <div>
-                <div className="p-4 text-start text-white bg-red-500 rounded-lg shadow-lg">
-                  {error}
+        <>
+          <div className="space-y-4 grid grid-cols-1 p-4">
+            {formik.values.configuration.map((c, index) => (
+              <>
+                <div>
+                  {balances.map((b, index) => (
+                    <div
+                      key={index}
+                      className="grid grid-cols-3 p-4 border border-gray-900 rounded-md"
+                    >
+                      <div className="flex flex-col items-start justify-center">
+                        <h5 className="text-start text-lg font-bold text-white">
+                          Balance #{index + 1}
+                        </h5>
+                      </div>
+                      <div className="flex flex-col items-start">
+                        <span className="font-semibold text-base text-white">
+                          Fecha Incio:
+                        </span>
+                        <p className="text-gray-400 text-sm">
+                          {FormatDate(b.fechaInicio, false)}
+                        </p>
+                      </div>
+                      <div className="flex flex-col items-start">
+                        <span className="font-semibold text-base text-white">
+                          Fecha Fin:
+                        </span>
+                        <p className="text-gray-400 text-sm">
+                          {FormatDate(b.fechaFin, false)}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              </div>
-            )}
-          </AnimatePresence>
-        </div>
+                <div
+                  key={index}
+                  className="grid md:grid-cols-2 grid-cols-1 gap-4 py-3 px-4"
+                >
+                  <Input
+                    type="number"
+                    label={`Días Tomados para Balance ID: ${c.balanceId}`}
+                    name={`configuration[${index}].diasTomados`}
+                    value={formik.values.configuration[index].diasTomados}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={getFieldError(index, "diasTomados")}
+                    touched={formik.touched.configuration?.[index]?.diasTomados}
+                    required
+                  />
+                  <Input
+                    label="Notas (Opcional)"
+                    name={`configuration[${index}].notas`}
+                    value={formik.values.configuration[index].notas}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={getFieldError(index, "notas")}
+                    touched={formik.touched.configuration?.[index]?.notas}
+                  />
+                </div>
+              </>
+            ))}
+            <AnimatePresence>
+              {error && (
+                <div>
+                  <div className="p-4 text-start text-white bg-red-500 rounded-lg shadow-lg">
+                    {error}
+                  </div>
+                </div>
+              )}
+            </AnimatePresence>
+          </div>
+        </>
       </FormModal>
     </>
   );
