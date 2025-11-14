92c38d320445aa0b2e74af4827ee3575a183e657
Gabriel Duarte <duartearias2002@gmail.com>
2025-11-14 11:01:13 -0500

    feat: agregar paginación y filtros a la tabla de balances en TableBalancesVacations

--- src/featuures/ConfigurationVacations/Page/TableBalancesVacations.tsx
+++ src/featuures/ConfigurationVacations/Page/TableBalancesVacations.tsx
@@ -1,9 +1,35 @@
 import LoadingSpinner from "@/components/common/LoadingSpinner/LoadingSpinner";
 import { useFetchBalances } from "../Hooks/useFetchBalances";
 import ModalConfigBalance from "../Components/ModalConfigBalance";
+import React, { useState } from "react";
+import useSearch from "@/hooks/useSearch";
+import { Datum } from "../Types/IBalancesVacations";
+import usePagination from "@/hooks/usePagination";
+import Input from "@/components/common/Ui/Input";
+import Select from "@/components/common/Ui/Select";
+import Pagination from "@/components/common/PaginationTable/PaginationTable";
 
 const TableBalancesVacations = () => {
   const { data, loading, error, refetch } = useFetchBalances();
+
+  const ITEMS_PER_PAGE = 10;
+
+  const [itemsPerPage] = useState(ITEMS_PER_PAGE);
+
+  const { query, setQuery, filteredData } = useSearch<Datum>(data?.data || [], [
+    "balances",
+    "userName",
+    "setupId",
+  ]);
+
+  const { currentPage, totalPages, paginate, currentData, setItemsPerPage } =
+    usePagination<Datum>(filteredData, itemsPerPage);
+
+  const handleItemsPerPageChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setItemsPerPage(Number(e.target.value));
+  };
 
   return (
     <>
@@ -15,6 +41,20 @@ const TableBalancesVacations = () => {
         <div className="w-full max-w-full p-5 ml-0 bg-white rounded-md shadow-lg dark:bg-gray-800 mb-11 shadow-indigo-500/10">
           <div className="flex items-center my-5 gap-3">
             {/* filtros de la tabla1 */}
+            <Input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Buscar"
+            />
+            <Select
+              options={[
+                { label: "10", value: "10" },
+                { label: "25", value: "25" },
+                { label: "50", value: "50" },
+              ]}
+              onChange={handleItemsPerPageChange}
+              value={itemsPerPage}
+            />
           </div>
           <table className="w-full">
             <thead className="text-gray-400 text-base">
@@ -26,7 +66,7 @@ const TableBalancesVacations = () => {
               <th className="text-end">Balances</th>
             </thead>
             <tbody>
-              {data?.data?.map((b, index) => (
+              {currentData().map((b, index) => (
                 <tr
                   className="border-b border-gray-200 dark:border-gray-700"
                   key={index}
@@ -47,12 +87,21 @@ const TableBalancesVacations = () => {
                     {b.observaciones}
                   </td>
                   <td className="text-end py-4 text-gray-400 p-0 border-gray-500 dark:text-gray-500 dark:bg-gray-800">
-                    <ModalConfigBalance balances={b.balances} onSuccess={refetch} userId={b.userId} />
+                    <ModalConfigBalance
+                      balances={b.balances}
+                      onSuccess={refetch}
+                      userId={b.userId}
+                    />
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={paginate}
+          />
         </div>
       )}
     </>
