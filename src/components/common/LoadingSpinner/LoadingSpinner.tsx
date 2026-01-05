d3d99811baf03f96bf5763ba0d4b5076817973cc
Gabriel Duarte <duartearias2002@gmail.com>
2026-01-05 17:05:43 -0500

    fix: eliminar fondo negro en LoadingSpinner y Modal para mejorar la apariencia

--- src/components/common/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -28,7 +28,7 @@ const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
 
   return (
     <div
-      className={`absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 transition-opacity duration-700 ease-in-out ${
+      className={`absolute inset-0 flex items-center justify-center bg-opacity-50 z-50 transition-opacity duration-700 ease-in-out ${
         fadeOut ? "opacity-0" : "opacity-100"
       }`}
     >
